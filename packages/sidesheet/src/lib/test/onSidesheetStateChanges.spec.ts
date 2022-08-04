dca87658dc67cc823c74c847d1171608904c5a1d
Gustav-Eikaas <guei@equinor.com>
2022-08-04 10:41:20 +0200

    added tests

---
+++ packages/sidesheet/src/lib/test/onSidesheetStateChanges.spec.ts
@@ -0,0 +1,18 @@
+import { SidesheetController } from "../classes"
+
+
+describe("Sidesheet state changes Closed/Open", () => {
+    it("Should call the callback function with Open", () => {
+        const mockFunction = jest.fn();
+
+        const controller = new SidesheetController();
+        expect(controller.isSidesheetOpen()).toBeFalsy();
+        controller.onSidesheetOpen((s) => {
+            expect(s === "Open").toBeTruthy();
+            mockFunction();
+        })
+        controller.setSidesheetState("Open");
+        expect(mockFunction).toBeCalled();
+
+    })
+})
