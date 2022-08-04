fd66a329a7f5e55321b44cf08d686ef0a8717b3a
Gustav-Eikaas <guei@equinor.com>
2022-08-04 12:14:59 +0200

    removed default nx files

--- packages/workspace-core/src/lib/workspace-core.spec.ts
+++
@@ -1,7 +0,0 @@
-import { workspaceCore } from './workspace-core';
-
-describe('workspaceCore', () => {
-  it('should work', () => {
-    expect(workspaceCore()).toEqual('workspace-core');
-  });
-});
--- packages/workspace-core/src/lib/workspace-core.ts
+++
@@ -1,3 +0,0 @@
-export function workspaceCore(): string {
-  return 'workspace-core';
-}
