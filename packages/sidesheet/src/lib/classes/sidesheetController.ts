6c52dfb1ef1c1fe8e924c5972d8044c1098b2e1b
Gustav-Eikaas <guei@equinor.com>
2022-08-08 13:48:48 +0200

    add comments to sidesheet controller

--- packages/sidesheet/src/lib/classes/sidesheetController.ts
+++ packages/sidesheet/src/lib/classes/sidesheetController.ts
@@ -8,18 +8,27 @@ import {
 } from '../types';
 
 export class SidesheetController<T> {
+    /** The item currently being held by the controller */
     item: T | undefined;
+    /** Current visual state of the sidesheet */
     sidesheetState: SidesheetState = 'Closed';
+    /** Function that returns the sidesheet state as a boolean */
     isSidesheetOpen = () => this.sidesheetState === 'Open';
 
-    onSidesheetStateChangedCallbacks: Callback<SidesheetChangedCallback<T>>[] = [];
-    onItemChangedCallbacks: Callback<ItemChangedCallback<T>>[] = [];
+    private onSidesheetStateChangedCallbacks: Callback<SidesheetChangedCallback<T>>[] = [];
+    private onItemChangedCallbacks: Callback<ItemChangedCallback<T>>[] = [];
 
+    /** Function that both closes the sidesheet and removes the item */
     closeAndRemoveItem = () => {
         this.setSidesheetState('Closed');
         this.setItem(undefined);
     };
 
+    /**
+     * Register a callback to be called upon when the sidesheet state changes
+     * @param cb 
+     * @returns 
+     */
     onSidesheetStateChanged = (cb: SidesheetChangedCallback<T>): OnCallbackSet => {
         const id = generateUniqueId();
         this.onSidesheetStateChangedCallbacks.push({ id, callback: cb });
@@ -32,7 +41,14 @@ export class SidesheetController<T> {
         };
     };
 
+    /**
+     * Function for opening or closing the sidesheet
+     * @param state 
+     * @param preventCallbacks 
+     */
     setSidesheetState = (state: SidesheetState, preventCallbacks?: boolean) => {
+        /** Wont bother the callbacks if the state didnt actually change */
+        if(state === this.sidesheetState) return;
         this.sidesheetState = state;
 
         if (!preventCallbacks) {
@@ -40,6 +56,11 @@ export class SidesheetController<T> {
         }
     };
 
+        /**
+         * Function for setting a new item for the sidesheet controller to hold
+         * @param item 
+         * @param preventCallbacks If true, prevents the callbacks from being notified of this change. Dont use unless absolutely necessary
+         */
     setItem = (item: T | undefined, preventCallbacks?: boolean) => {
         this.item = item;
         if (!preventCallbacks) {
@@ -47,6 +68,12 @@ export class SidesheetController<T> {
         }
     };
 
+
+    /**
+     * Callback to be called on whenever the item changes
+     * @param cb function to call whenever the item changes
+     * @returns 
+     */
     onItemChanges = (cb: ItemChangedCallback<T>): OnCallbackSet => {
         const id = generateUniqueId();
         this.onItemChangedCallbacks.push({
