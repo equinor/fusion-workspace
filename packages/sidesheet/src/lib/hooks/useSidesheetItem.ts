0acd41cc03fea667b85c943b40ffc4d984e42e8f
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2022-08-09 08:44:23 +0200

    Add sidesheet controller

---
+++ packages/sidesheet/src/lib/hooks/useSidesheetItem.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { SidesheetController } from "../classes";
+
+export function useSidesheetItem<TItem, TContext = any>({item, onItemChanges}: SidesheetController<TItem, TContext>){
+    const [sidesheetItem, setItem] = useState<TItem | undefined>(item);
+
+
+    useEffect(() =>{
+        const { unsubscribe } = onItemChanges(setItem)
+        return unsubscribe;
+    },[onItemChanges])
+
+
+    return sidesheetItem;
+}
