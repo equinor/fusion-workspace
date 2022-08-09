f2e9b4559138d20a7b6c85d2ae73cf2ec3d239a4
Gustav-Eikaas <guei@equinor.com>
2022-08-09 08:50:47 +0200

    update comments onDataChanged test

--- packages/data-source/src/lib/test/onDataChanged.spec.ts
+++ packages/data-source/src/lib/test/onDataChanged.spec.ts
@@ -5,37 +5,35 @@ async function defaultFetch(){
 }
 const mocFunction = jest.fn();
 
-describe("OnDataChangedTesting", () => {
-
-
+describe("Validates that the onDataChanged event fires correctly", () => {
     const controller =  new DataSourceController(defaultFetch);
     controller.responseParserAsync = async () => {
         return [1,2,3]
     }
 
     it("Should run onDataChangedCallback", async () => {
-        const {unSubscribe} = controller.onDataChanged((s) => {
+        const { unsubscribe } = controller.onDataChanged((s) => {
             mocFunction();
             expect(s.length).toBe(3)
         })
         await controller.fetchData();
         expect(mocFunction).toBeCalledTimes(1)
-        unSubscribe();
+        unsubscribe();
     });
+
     it("Should not run onDataChanged when preventCallbacks is set to true", async () => {
-        const {unSubscribe} = controller.onDataChanged(() => {
+        const { unsubscribe } = controller.onDataChanged(() => {
+            /** If this callback is called the test will fail */
             throw "Function should not have been called, fetchData is ignoring the preventCallbacks flag"
         })
         await controller.fetchData(true);
-        unSubscribe();
-    })
-
-
+        unsubscribe();
+    });
 
     it("Should remove the data", () => {
         expect(controller.data.length).toBeTruthy();
         controller.remove();
         expect(controller.data.length).toBeFalsy();
-    })
+    });
 
 })
