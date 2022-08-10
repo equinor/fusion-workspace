b3e6ea3e53b2de1f62c8bcded6f1df32b9df34bc
Gustav-Eikaas <guei@equinor.com>
2022-08-10 13:11:07 +0200

    refactor

--- packages/workspace-react/src/lib/components/workspaceHeader/WorkspaceHeader.tsx
+++ packages/workspace-react/src/lib/components/workspaceHeader/WorkspaceHeader.tsx
@@ -7,14 +7,14 @@ import { StyledWorkspaceHeader } from './workspaceHeader.styles';
  * Workspace header component.
  * Contains tab navigation and status bar
  */
-export function WorkspaceHeader<TTabnames extends string, TError>({controller: {statusBarItems, FilterComponent, setActiveTab, tabs}}: WorkspaceProps<TTabnames, TError>) {
+export function WorkspaceHeader<TTabnames extends string, TError>({controller}: WorkspaceProps<TTabnames, TError>) {
 
     return (
         <StyledWorkspaceHeader>
-            <StatusBar items={statusBarItems ?? []} />
-            <TabNavigation setActiveTab={setActiveTab} tabs={tabs} />
-            {FilterComponent && (
-                <FilterComponent />
+            <StatusBar items={controller.statusBarItems ?? []} />
+            <TabNavigation controller={controller} />
+            {controller.FilterComponent && (
+                <controller.FilterComponent />
             )}
         </StyledWorkspaceHeader>
     );
