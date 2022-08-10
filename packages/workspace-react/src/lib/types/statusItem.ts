5b327fd131190cff6954bb963dd8740231df062d
Gustav-Eikaas <guei@equinor.com>
2022-08-10 13:10:25 +0200

    remove weekly change

--- packages/workspace-react/src/lib/types/statusItem.ts
+++ packages/workspace-react/src/lib/types/statusItem.ts
@@ -3,7 +3,6 @@ export interface StatusItem {
     title: string;
     /** Value to be shown in the status bar */
     value: string | number;
-    /** Value that reflects how much the main value has changed over the course of 1 week */
-    weeklyChange?: () => (string | number);
+    /** Added as HTML title */
     description?: string;
 }
