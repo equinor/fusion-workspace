9e1ca450754e98a9666a15ea4a32494aec699625
Gustav-Eikaas <guei@equinor.com>
2022-08-10 14:28:13 +0200

    add gridwrapper styled component

--- packages/ag-grid/src/lib/components/Grid.tsx
+++ packages/ag-grid/src/lib/components/Grid.tsx
@@ -4,6 +4,7 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 import { GridController } from '../classes/gridController';
 import { useRowData } from '../hooks/useRowData';
+import { StyledGridWrapper } from './grid.styles';
 
 interface GridProps<T> {
     controller: GridController<T>;
@@ -11,12 +12,12 @@ interface GridProps<T> {
 
 export function Grid<T>({ controller }: GridProps<T>) {
     return (
-        <div className="ag-theme-alpine" style={{ height: 1000, width: "100%" }}>
+        <StyledGridWrapper className="ag-theme-alpine" >
             <AgGridReact
                 gridOptions={controller.gridOptions}
                 columnDefs={controller.columnDefs}
                 rowData={useRowData(controller)}
             />
-        </div>
+        </StyledGridWrapper>
     );
 }
