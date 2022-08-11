e1899c1f1b80c60e10e61e60850ec799e1a91d57
Gustav-Eikaas <guei@equinor.com>
2022-08-11 14:16:57 +0200

    types

--- packages/workspace-react/src/lib/hooks/index.ts
+++ packages/workspace-react/src/lib/hooks/index.ts
@@ -2,3 +2,4 @@ export { useEffectOnce } from './useEffectOnce';
 export { useIsLoading } from './useIsLoading';
 export { useIsSidesheetOpen } from './useIsSidesheetOpen';
 export { useSidesheetWidth } from './useSidesheetWidth';
+export { useActiveTab } from './useActiveTab';
