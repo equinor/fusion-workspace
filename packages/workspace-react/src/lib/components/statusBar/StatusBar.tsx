293971ae1bb0c47e3d5542e846ef525f1f0a94fa
Gustav-Eikaas <guei@equinor.com>
2022-08-11 14:15:47 +0200

    add Castberg styling to status bar

--- packages/workspace-react/src/lib/components/statusBar/StatusBar.tsx
+++ packages/workspace-react/src/lib/components/statusBar/StatusBar.tsx
@@ -1,21 +1,20 @@
 import { StatusItem } from '../../types';
+import { StatusBarItem } from './StatusBarItem';
 
 interface StatusBarProps {
 	items: StatusItem[];
 }
 
-export function StatusBar({ items }: StatusBarProps): JSX.Element | null {
+export function StatusBar({ items }: StatusBarProps): JSX.Element {
 	if (!items.length) {
-		return null;
+		//Returns div for styling purposes
+		return <div></div>;
 	}
 
 	return (
-		<div style={{ display: 'flex', gap: '2em' }}>
-			{items?.map((s) => (
-				<div title={s.description} key={s.title}>
-					<div>{s.title}</div>
-					<div>{s.value}</div>
-				</div>
+		<div style={{ display: 'flex', gap: '2em', margin: '0px 32px' }}>
+			{items?.map((item) => (
+				<StatusBarItem key={item.title} item={item} />
 			))}
 		</div>
 	);
---
+++ packages/workspace-react/src/lib/components/statusBar/StatusBarItem.tsx
@@ -0,0 +1,16 @@
+import { StatusItem } from '../../types';
+import { StatusCard, Title, ValueWrapper, Value } from './statusBarItem.styles';
+
+interface StatusBarItemProps {
+	item: StatusItem;
+}
+export function StatusBarItem({ item }: StatusBarItemProps) {
+	return (
+		<StatusCard title={item.description} key={item.title}>
+			<Title>{item.title}</Title>
+			<ValueWrapper>
+				<Value>{item.value}</Value>
+			</ValueWrapper>
+		</StatusCard>
+	);
+}
