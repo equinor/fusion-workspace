43f1199316c7a10ab52974ae45eb5d2c3f6094f5
Gustav-Eikaas <guei@equinor.com>
2022-08-11 14:17:06 +0200

    refactor workspaceTab

--- packages/workspace-react/src/lib/components/workspaceTab/WorkspaceTab.tsx
+++ packages/workspace-react/src/lib/components/workspaceTab/WorkspaceTab.tsx
@@ -1,27 +1,11 @@
-import { useEffect, useState } from 'react';
-import { Tab } from '../../types';
+import { useActiveTab } from '../../hooks';
 import { WorkspaceProps } from '../Workspace';
 
 /**
  * Renders the current active workspace tab
  */
 export function WorkspaceTab<TabNames extends string, TError>({ controller }: WorkspaceProps<TabNames, TError>) {
-	const [tab, setTab] = useState<Tab<string> | undefined>(
-		controller.tabs.find((s) => s.name === controller.activeTab)
-	);
-
-	useEffect(() => {
-		const { unsubscribe } = controller.onActiveTabChanged((to) => {
-			const newTab = controller.tabs.find((s) => s.name === to);
-			if (!newTab) {
-				setTab(undefined);
-			} else {
-				setTab(newTab);
-			}
-		});
-		return unsubscribe;
-	}, []);
-
+	const tab = useActiveTab(controller);
 	if (!tab) return null;
 	const { Component } = tab;
 	return <Component />;
