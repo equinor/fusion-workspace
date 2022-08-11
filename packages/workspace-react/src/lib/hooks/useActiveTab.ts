6d36db6335f8b1edd8b9d9ed46f6ac362993595e
Gustav-Eikaas <guei@equinor.com>
2022-08-11 14:16:50 +0200

    useActiveTab hook

---
+++ packages/workspace-react/src/lib/hooks/useActiveTab.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import { WorkspaceViewController } from '../classes';
+import { Tab } from '../types';
+
+export function useActiveTab<TabNames extends string, TError>(controller: WorkspaceViewController<TabNames, TError>) {
+	const [tab, setTab] = useState<Tab<string> | undefined>(
+		controller.tabs.find((s) => s.name === controller.activeTab)
+	);
+
+	useEffect(() => {
+		const { unsubscribe } = controller.onActiveTabChanged((to) => {
+			const newTab = controller.tabs.find((s) => s.name === to);
+			if (!newTab) {
+				setTab(undefined);
+			} else {
+				setTab(newTab);
+			}
+		});
+		return unsubscribe;
+	}, []);
+
+	return tab;
+}
