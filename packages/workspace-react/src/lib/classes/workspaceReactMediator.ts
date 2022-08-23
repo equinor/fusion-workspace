b1feb6ec74083401b89c062265e442c7d013d3f8
Gustav-Eikaas <guei@equinor.com>
2022-08-23 13:53:40 +0200

    add onMount onUnmount events

--- packages/workspace-react/src/lib/classes/workspaceReactMediator.ts
+++ packages/workspace-react/src/lib/classes/workspaceReactMediator.ts
@@ -5,7 +5,7 @@ export class WorkspaceReactMediator<
 	TOnClick extends ObjectType<TOnClick> = ObjectType<unknown>,
 	TError extends ObjectType<TError> = ObjectType<unknown>,
 	TContext extends ObjectType<TContext> = ObjectType<unknown>
-> extends WorkspaceMediator<TData, TOnClick, TError, TContext> {
+> extends WorkspaceMediator<TData, TOnClick, TError> {
 	isSidesheetOpen = false;
 	setIsSidesheetOpen: (value: boolean) => void;
 	onSidesheetStateChange: (callback: OnchangeCallback<boolean>) => () => void;
@@ -13,6 +13,14 @@ export class WorkspaceReactMediator<
 	isLoading = false;
 	setIsLoading: (value: boolean) => void;
 	onIsLoadingChange: (callback: OnchangeCallback<boolean>) => () => void;
+
+	onMount: (callback: OnchangeCallback<boolean>) => () => void;
+
+	onUnMount: (callback: OnchangeCallback<boolean>) => () => void;
+
+	setMount: () => void;
+
+	setUnmount: () => void;
 
 	constructor() {
 		super();
@@ -29,5 +37,13 @@ export class WorkspaceReactMediator<
 		});
 		this.setIsSidesheetOpen = isSidesheetOpen.setValue;
 		this.onSidesheetStateChange = isSidesheetOpen.onchange;
+
+		const mounted = new Observable(false);
+		this.onMount = mounted.onchange;
+		this.setMount = () => mounted.setValue(!mounted.value);
+
+		const unMounted = new Observable(false);
+		this.onUnMount = unMounted.onchange;
+		this.setUnmount = () => unMounted.setValue(!mounted.value);
 	}
 }
