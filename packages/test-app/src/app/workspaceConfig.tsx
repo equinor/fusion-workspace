987cd7158d64a18edbf174e171b747532f497645
Gustav-Eikaas <guei@equinor.com>
2022-08-24 13:24:51 +0200

    make test app more realistic

--- packages/test-app/src/app/workspaceConfig.tsx
+++ packages/test-app/src/app/workspaceConfig.tsx
@@ -1,19 +1,45 @@
 import { GridConfig, SidesheetConfig, StatusBarConfig, WorkspaceOnClick } from '@equinor/workspace-fusion';
 import { Handover } from './types';
 import { mockData } from './mockData';
+import styled from 'styled-components';
 
 export const gridOptions: GridConfig<Handover> = {
 	columnDefinitions: [
 		{ field: 'commPkgNo', valueGetter: (s) => s.data?.commpkgNo },
 		{ field: 'Description', valueGetter: (s) => s.data?.description },
 		{ field: 'Disciplines', valueGetter: (s) => s.data?.mcDisciplineCodes },
-		{ field: 'Comm pkg status', valueGetter: (s) => s.data?.commpkgStatus },
-		{ field: 'MC status', valueGetter: (s) => s.data?.mcStatus },
+		{
+			field: 'Comm pkg status',
+			valueGetter: (s) => s.data?.commpkgStatus,
+			cellRenderer: (props) => RenderStatus(props.valueFormatted ?? props.value),
+		},
+		{
+			field: 'MC status',
+			valueGetter: (s) => s.data?.mcStatus,
+			cellRenderer: (props) => RenderStatus(props.valueFormatted ?? props.value),
+		},
 		{ field: 'Responsible', valueGetter: (s) => s.data?.responsible },
 		{ field: 'Area', valueGetter: (s) => s.data?.area },
 		{ field: 'System', valueGetter: (s) => s.data?.system },
 	],
 };
+
+/** Will render MC or Comm pkg status symbol */
+const RenderStatus = (value: string) => {
+	return (
+		<span style={{ display: 'flex', alignItems: 'center' }}>
+			<StyledCircle color={value === 'OK' ? 'green' : 'grey'} />
+			{value}
+		</span>
+	);
+};
+
+const StyledCircle = styled.div<{ color: string }>`
+	height: 16px;
+	width: 16px;
+	background-color: ${({ color }) => color};
+	border-radius: 50%;
+`;
 
 export const customTab = {
 	Component: ({ data, onClick }) => <div onClick={() => onClick({ item: data[0] })}>{data.length}</div>,
@@ -34,7 +60,13 @@ export const dataSourceOptions = async (): Promise<Handover[]> =>
 	});
 
 export const statusBarConfig: StatusBarConfig<Handover> = [
-	{ getValue: (data) => ({ title: 'Count', value: data.length }) },
+	{ getValue: (data) => ({ title: 'Total CP', value: data.length }) },
+	{
+		getValue: (data) => ({
+			title: 'RFO accepted',
+			value: data.map((s) => s.rfocIsAccepted).reduce((prev, curr) => (prev = prev + (curr ? 1 : 0)), 0),
+		}),
+	},
 ];
 
 export function SidesheetComponent(ev: WorkspaceOnClick<Handover>) {
