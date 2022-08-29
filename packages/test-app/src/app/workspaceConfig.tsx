2292cd5ba1dce687e3b6f5cc426b65f9991d4056
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2022-08-29 11:55:13 +0200

    Table config testapp (#58)
    * extend tableconfig test app
    
    * add pagination to grid

--- packages/test-app/src/app/workspaceConfig.tsx
+++ packages/test-app/src/app/workspaceConfig.tsx
@@ -28,7 +28,18 @@ export const gridOptions: GridConfig<Handover> = {
 		{ field: 'Responsible', valueGetter: (s) => s.data?.responsible },
 		{ field: 'Area', valueGetter: (s) => s.data?.area },
 		{ field: 'System', valueGetter: (s) => s.data?.system },
+		{ field: 'Priority 1', valueGetter: (s) => s.data?.priority1 },
+		{ field: 'Priority 2', valueGetter: (s) => s.data?.priority2 },
+		{ field: 'Priority 3', valueGetter: (s) => s.data?.priority3 },
+		{ field: 'Planned start date', valueGetter: (s) => s.data?.plannedStartDate, headerName: 'Planned RFC' },
+		{ field: 'forecastStartDate', headerName: 'Forecast RFC' },
+		{ field: 'rfocPlannedDate 3', headerName: 'Planned RFO' },
+		{ field: 'rfocActualDate', headerName: 'Actual RFO' },
 	],
+	gridOptions: {
+		pagination: true,
+		paginationPageSize: 100,
+	},
 };
 
 /** Will render MC or Comm pkg status symbol */
