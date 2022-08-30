a3769c02add2d090ab18528875fd5e8962f86fd5
Gustav-Eikaas <guei@equinor.com>
2022-08-30 09:00:30 +0200

    fix sidesheet color

--- packages/test-app/src/app/gardenConfig/appConfig.ts
+++ packages/test-app/src/app/gardenConfig/appConfig.ts
@@ -1,7 +1,7 @@
 import { AppConfig, WorkspaceTabNames } from '@equinor/workspace-fusion';
 
 export const appConfig: AppConfig<WorkspaceTabNames> = {
-	appColor: 'purple',
+	appColor: '#0084C4',
 	appKey: 'Handover',
 	defaultTab: 'grid',
 };
