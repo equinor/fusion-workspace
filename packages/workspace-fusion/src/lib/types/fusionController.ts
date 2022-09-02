79513fabf9a1a94019a910af7a8cdd3fa60f3e1c
Gustav-Eikaas <guei@equinor.com>
2022-09-02 09:58:02 +0200

    extract fusion bookmarks to file

--- packages/workspace-fusion/src/lib/types/fusionController.ts
+++ packages/workspace-fusion/src/lib/types/fusionController.ts
@@ -1,23 +1,7 @@
-import { GroupingKeys } from '@equinor/garden';
 import { WorkspaceReactMediator } from '@equinor/workspace-react';
 import { WorkspaceContext } from '../classes/fusionWorkspaceBuilder';
+import { FusionBookmark } from './fusionBookmark';
 import { WorkspaceOnClick } from './onClick';
-import { WorkspaceTabNames } from './tabs';
-
-export interface FusionBookmark<TData> {
-	grid?: any;
-	table?: any;
-	filter?: any;
-	powerBI?: any;
-	garden?: {
-		groupingKeys: GroupingKeys<TData>;
-	};
-	view?: ViewBookmark;
-}
-
-interface ViewBookmark {
-	activeTab?: WorkspaceTabNames;
-}
 
 /**
  * Workspace controller for fusion with some predefined types
