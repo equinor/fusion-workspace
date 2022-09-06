638827cac478b9f55bc66c52f4c00b91c82f4a28
Gustav-Eikaas <guei@equinor.com>
2022-09-06 15:34:43 +0200

    replace objectIdentifier with function to return unique id

--- packages/workspace-core/src/lib/classes/workspaceMediator.ts
+++ packages/workspace-core/src/lib/classes/workspaceMediator.ts
@@ -12,43 +12,44 @@ export class WorkspaceMediator<
 	TOnClick extends ObjectType<TOnClick> = ObjectType<unknown>,
 	TError extends ObjectType<TError> = ObjectType<unknown>
 > {
-	/**
-	 * Callback that returns an instance of itself
-	 * Helpful when chaining
-	 */
-	addMiddleware = (cb: (mediator: this) => void) => {
-		cb(this);
-		return this;
-	};
-	selection = new SelectionService();
-	urlService = new URLService();
+	/** Returns a unique ID for a given item */
+	getUniqueId: GetIdentifier<TData>;
 
 	/** Register a callback to be called when filtered data changes*/
 	onDataChange: (callback: OnchangeCallback<TData[]>) => () => void;
+
 	/** The data used for the workspace */
 	data: TData[] | undefined;
+
 	/** Sets the data */
 	setData: (value: TData[]) => void;
 
 	/** Register a callback to be called when filtered data changes*/
 	onFilterDataChange: (callback: OnchangeCallback<TData[]>) => () => void;
+
 	/** The filtered data used for the workspace */
 	filteredData: TData[] | undefined;
+
 	/** Sets the filtered data */
 	setFilteredData: (value: TData[]) => void;
 
 	/** Value of the last click event */
 	lastClick?: TOnClick;
+
 	/** Triggers a clickevent */
 	click: (clickEv: TOnClick) => void;
+
 	/** Register a callback to be called when click is triggered */
 	onClick: (callback: OnchangeCallback<TOnClick>) => () => void;
+
 	/** Triggers an error event */
 	throwError: (error: TError) => void;
+
 	/** Register a callback to be called when an error occurs */
 	onError: (cb: (error: TError) => void) => void;
 
-	constructor() {
+	constructor(getUniqueId: GetIdentifier<TData>) {
+		this.getUniqueId = getUniqueId;
 		const data = new Observable<TData[]>();
 		this.onDataChange = data.onchange;
 		data.onchange((val) => {
@@ -75,6 +76,21 @@ export class WorkspaceMediator<
 		this.onError = error.onchange;
 	}
 
+	/**
+	 * Callback that returns an instance of itself
+	 * Helpful when chaining
+	 */
+	addMiddleware = (cb: (mediator: this) => void) => {
+		cb(this);
+		return this;
+	};
+
+	/**Service for keeping track of selected items */
+	selection = new SelectionService();
+
+	/**Service for keeping track of url changes */
+	urlService = new URLService();
+
 	/** Call this function when mediator should be destroyed */
 	destroy = () => {
 		for (const key in this) {
@@ -83,3 +99,5 @@ export class WorkspaceMediator<
 		}
 	};
 }
+
+export type GetIdentifier<TData> = (item: TData) => string;
