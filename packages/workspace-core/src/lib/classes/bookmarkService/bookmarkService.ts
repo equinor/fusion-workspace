430574e6fdded42d481ef02423176dd1c94ebace
Gustav-Eikaas <guei@equinor.com>
2022-09-06 08:32:56 +0200

    add docs

--- packages/workspace-core/src/lib/classes/bookmarkService/bookmarkService.ts
+++ packages/workspace-core/src/lib/classes/bookmarkService/bookmarkService.ts
@@ -3,16 +3,23 @@ export class BookmarkService<TBookmarkState extends Record<PropertyKey, unknown>
 	private onApplyCallbacks: OnCapture<TBookmarkState>[] = [];
 	private captureCallbacks: CaptureFunc<TBookmarkState>[] = [];
 
+	/**Subscribe to bookmark capture events */
 	onCapture = (cb: OnCapture<TBookmarkState>) => {
 		this.onCaptureCallbacks.push(cb);
 	};
 
+	/**
+	 * Subscribe to apply events
+	 * @param cb Function to be called when someone applies a bookmark
+	 */
 	onApply = (cb: OnCapture<TBookmarkState>) => {
 		this.onApplyCallbacks.push(cb);
 	};
 
+	/**Applies a bookmark state to all the subscribers listening on onApply event */
 	apply = (state: TBookmarkState) => this.onApplyCallbacks.forEach((s) => s(state));
 
+	/** Captures a bookmark using all the registered capturers */
 	capture = () => {
 		const bookmarkState = this.captureCallbacks.reduce(
 			(state, callback) => (state = { ...state, ...callback() }),
@@ -22,6 +29,7 @@ export class BookmarkService<TBookmarkState extends Record<PropertyKey, unknown>
 		return bookmarkState;
 	};
 
+	/** Register a function to capture a partial bookmark state */
 	registerCapture(cb: CaptureFunc<TBookmarkState>) {
 		this.captureCallbacks.push(cb);
 	}
