3b75b3dc6f7f9f0816f74698e0d36729e8d4c485
Gustav-Eikaas <guei@equinor.com>
2022-09-06 08:35:10 +0200

    docs

--- packages/workspace-fusion/src/lib/utils/garden/configureDataChange.ts
+++ packages/workspace-fusion/src/lib/utils/garden/configureDataChange.ts
@@ -1,6 +1,7 @@
 import { GardenController } from '@equinor/garden';
 import { FusionMediator } from '../../types';
 
+/** Updates data on gardencontroller whenever filtered data on mediator changes */
 export function configureDataChange<TData, TError, TCustomGroupByKeys, TCustomState, TContext>(
 	gardenController: GardenController<TData, TCustomGroupByKeys, TCustomState, TContext>,
 	mediator: FusionMediator<TData, TError>
