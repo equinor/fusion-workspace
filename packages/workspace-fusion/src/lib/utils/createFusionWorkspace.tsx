eb148dd322812a65c6637bef3d66a8aaa7f016d6
Gustav-Eikaas <guei@equinor.com>
2022-09-07 13:22:14 +0200

    define appkey for fusionworkspace

--- packages/workspace-fusion/src/lib/utils/createFusionWorkspace.tsx
+++ packages/workspace-fusion/src/lib/utils/createFusionWorkspace.tsx
@@ -5,11 +5,13 @@ type UserConfig<TData, TError> = (
 	builder: FusionWorkspaceBuilder<TData, TError>
 ) => FusionWorkspaceBuilder<TData, TError>;
 
-export function createFusionWorkspace<TData, TError>(
-	objectIdentifier: keyof TData,
-	builderFunc: UserConfig<TData, TError>
-) {
-	const builder = builderFunc(new FusionWorkspaceBuilder(objectIdentifier));
+export function createFusionWorkspace<TData, TError>(config: AppConfig<TData>, builderFunc: UserConfig<TData, TError>) {
+	const builder = builderFunc(new FusionWorkspaceBuilder(config.objectIdentifier, config.appKey));
 
 	return sortFusionTabs(builder.viewController);
+}
+
+interface AppConfig<TData> {
+	objectIdentifier: keyof TData;
+	appKey: string;
 }
