537df9832e5566bfd63a1cc0539514ecd675f108
Gustav-Eikaas <guei@equinor.com>
2022-09-07 13:22:03 +0200

    test-app

--- packages/test-app/src/app/app.tsx
+++ packages/test-app/src/app/app.tsx
@@ -1,31 +1,36 @@
 import { createFusionWorkspace } from '@equinor/workspace-fusion';
+import { IndexedDbModule } from '@equinor/workspace-fusion-modules';
 import { Workspace } from '@equinor/workspace-react';
 import { Handover } from './types';
 import { customTab, dataSourceOptions, gridOptions, sidesheetOptions, statusBar } from './workspaceConfig';
 
-const controller = createFusionWorkspace<Handover, unknown>('commpkgNo', ({ addDataSource }) =>
-	addDataSource(dataSourceOptions)
-		.addGrid(gridOptions)
-		.addCustomTab(customTab)
-		.addConfig({
-			appColor: 'purple',
-			appKey: 'Handover',
-			defaultTab: 'grid',
-		})
-		.addSidesheet(sidesheetOptions)
-		.addGarden({
-			data: [],
-			nodeLabelCallback: (s) => s.commpkgNo,
-			objectIdentifier: 'commpkgNo',
-			initialGrouping: { horizontalGroupingAccessor: 'id', verticalGroupingKeys: [] },
-			fieldSettings: {},
-		})
-		.addMiddleware((mediator) => {
-			mediator.urlService.onUrlChange((val) => {
-				console.log('app url changed', val);
-			});
-		})
-		.addStatusBarItems(statusBar)
+const controller = createFusionWorkspace<Handover, unknown>(
+	{ appKey: 'Handover', objectIdentifier: 'commpkgNo' },
+	(builder) =>
+		builder
+			.addDataSource(dataSourceOptions)
+			.addGrid(gridOptions)
+			.addCustomTab(customTab)
+			.addConfig({
+				appColor: 'purple',
+				appKey: 'Handover',
+				defaultTab: 'grid',
+			})
+			.addSidesheet(sidesheetOptions)
+			.addGarden({
+				data: [],
+				nodeLabelCallback: (s) => s.commpkgNo,
+				objectIdentifier: 'commpkgNo',
+				initialGrouping: { horizontalGroupingAccessor: 'id', verticalGroupingKeys: [] },
+				fieldSettings: {},
+			})
+			.addModules([IndexedDbModule])
+			.addMiddleware((mediator) => {
+				mediator.urlService.onUrlChange((val) => {
+					console.log('app url changed', val);
+				});
+			})
+			.addStatusBarItems(statusBar)
 );
 
 function FusionWorkspace() {
