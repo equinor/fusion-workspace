559b09474c62bd97bcc2abb556a2cedb0eb5de68
Gustav-Eikaas <guei@equinor.com>
2022-09-08 13:50:40 +0200

    merge conflict

--- packages/workspace-fusion/src/lib/utils/dataSource/addDataSource.ts
+++ packages/workspace-fusion/src/lib/utils/dataSource/addDataSource.ts
@@ -6,13 +6,7 @@ export function addDataSource<TData, TError>(
 	{ dataService, setIsLoading }: FusionMediator<TData, TError>
 ) {
 	const dataSourceController = new FetchController(dataFetch);
-	dataSourceController.onDataChanged((data) => {
-		dataService.setData(data);
-		//TEMP: Will be removed once filter is done
-		dataService.setFilteredData(data);
-	});
-	dataSourceController.onIsLoadingChanged((isLoading) => {
-		setIsLoading(isLoading);
-	});
+	dataSourceController.onDataChanged(dataService.setData);
+	dataSourceController.onIsLoadingChanged(setIsLoading);
 	dataSourceController.getDataAsync();
 }
