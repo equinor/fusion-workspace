dfda4b7ef9cbd58301d33d3ca0f0e00d7f919ef3
Gustav-Eikaas <guei@equinor.com>
2022-09-08 12:02:59 +0200

    update tests

--- packages/data-source/src/lib/__tests__/fetchTest.spec.ts
+++ packages/data-source/src/lib/__tests__/fetchTest.spec.ts
@@ -29,4 +29,44 @@ describe('Fetch controller should handle cancellation and spam', () => {
 		await fetchController.getDataAsync();
 		expect(mockFunction).toBeCalledTimes(2);
 	});
+
+	it('Should correctly set loading states', async () => {
+		const mockFunction = jest.fn();
+
+		const { getDataAsync, onIsLoadingChanged } = basicTestStructure();
+		onIsLoadingChanged(mockFunction);
+		await getDataAsync();
+		await getDataAsync();
+		expect(mockFunction).toBeCalledTimes(2);
+	});
+
+	it('Should set isFetching state correctly', async () => {
+		const mockFunction = jest.fn();
+		const { getDataAsync, onIsFetchingChanged } = basicTestStructure();
+		onIsFetchingChanged(mockFunction);
+		await getDataAsync();
+		await getDataAsync();
+		expect(mockFunction).toBeCalledTimes(4);
+	});
+
+	it('Should reset states if fetch throws error', async () => {
+		const fetch = () => {
+			throw new Error('Something went wrong');
+		};
+		const { getDataAsync, isLoading, isFetching } = new FetchController(fetch);
+		try {
+			await getDataAsync();
+			// eslint-disable-next-line no-empty
+		} catch (e) {
+		} finally {
+			expect(isLoading).toBeFalsy();
+			expect(isFetching).toBeFalsy();
+		}
+	});
 });
+
+/**Creates a new fetch controller */
+function basicTestStructure() {
+	const fetch = () => [];
+	return new FetchController(fetch);
+}
