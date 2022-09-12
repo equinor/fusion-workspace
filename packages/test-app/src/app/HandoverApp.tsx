aa38f0923aea8cfbb92efc0750334b7d4dd598d3
Gustav-Eikaas <guei@equinor.com>
2022-09-12 08:49:29 +0200

    add fusion data fetch

--- packages/test-app/src/app/HandoverApp.tsx
+++ packages/test-app/src/app/HandoverApp.tsx
@@ -20,15 +20,19 @@ export function HandoverApp() {
 const createWorkspaceController = (client: HttpClientMsal) => {
 	return createFusionWorkspace<Handover, unknown>(
 		{ appKey: 'Handover', getIdentifier: (item) => item.commpkgNo },
-		({ addDataSource }) =>
-			addDataSource(async (signal) => {
-				return await (
-					await client.fetch(
-						'https://pro-s-dataproxy-ci.azurewebsites.net/api/contexts/94dd5f4d-17f1-4312-bf75-ad75f4d9572c/handover',
-						{ signal }
-					)
-				).json();
-			})
+		(builder) =>
+			builder
+				.addDataSource({
+					getResponseAsync: async (signal) => {
+						return await client.fetch(
+							'https://pro-s-dataproxy-ci.azurewebsites.net/api/contexts/94dd5f4d-17f1-4312-bf75-ad75f4d9572c/handover',
+							{ signal }
+						);
+					},
+					responseParser: async (res) => {
+						return await res.json();
+					},
+				})
 				.addGrid(gridOptions)
 				.addCustomTab(customTab)
 				.addConfig({
