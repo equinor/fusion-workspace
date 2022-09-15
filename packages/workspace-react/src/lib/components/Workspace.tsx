c35ba6aafe169e6cd7d3a01a5a819f836dd279b8
Gustav-Eikaas <guei@equinor.com>
2022-09-15 09:37:55 +0200

    add new header

--- packages/workspace-react/src/lib/components/Workspace.tsx
+++ packages/workspace-react/src/lib/components/Workspace.tsx
@@ -1,20 +1,41 @@
 import { WorkspaceViewController } from '../classes/workspaceViewController';
+import { useActiveTab } from '../hooks';
 import { useMounted } from '../hooks/useMounted';
 import { ContextProvider } from './ContextProvider';
+import { WorkspaceProviders } from './providers/WorkspaceProviders';
+import { TabNavigation } from './tabNavigation';
 import { WorkspaceBody } from './workspaceBody';
-import { WorkspaceHeader } from './workspaceHeader';
 export interface WorkspaceProps<TTabName extends string, TError> {
 	controller: WorkspaceViewController<TTabName, TError>;
 }
 
 export function Workspace<TTabNames extends string, TError>({ controller }: WorkspaceProps<TTabNames, TError>) {
 	useMounted(controller);
+
 	return (
 		<div style={{ height: '100%', display: 'grid', gridTemplateRows: 'auto 1fr' }}>
 			<ContextProvider controller={controller}>
-				<WorkspaceHeader />
-				<WorkspaceBody />
+				<WorkspaceProviders>
+					<Header />
+					<WorkspaceBody />
+				</WorkspaceProviders>
 			</ContextProvider>
+		</div>
+	);
+}
+
+function Header() {
+	const tab = useActiveTab();
+	if (!tab || !tab.CustomHeader)
+		return (
+			<div>
+				<TabNavigation />
+			</div>
+		);
+
+	return (
+		<div>
+			<tab.CustomHeader />
 		</div>
 	);
 }
