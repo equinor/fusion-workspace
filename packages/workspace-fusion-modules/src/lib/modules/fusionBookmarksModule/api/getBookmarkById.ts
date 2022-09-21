0436c3664df0a20fff0d818002150c09e159f90c
Gustav-Eikaas <guei@equinor.com>
2022-09-21 14:40:06 +0200

    add error handling

--- packages/workspace-fusion-modules/src/lib/modules/fusionBookmarksModule/api/getBookmarkById.ts
+++ packages/workspace-fusion-modules/src/lib/modules/fusionBookmarksModule/api/getBookmarkById.ts
@@ -7,6 +7,7 @@ import { getFusionClient } from '../utils/getClientAsync';
 export async function getBookmarkById(bookmarkId: string): Promise<BookmarkResponse> {
 	const client = await getFusionClient();
 	const res = await client.fetch(`/bookmarks/${bookmarkId}`);
+	if (!res.ok) throw new Error('Failed to get bookmark');
 	return await res.json();
 }
 export type BookmarkResponse<TPayload = unknown> = {
--- packages/workspace-fusion-modules/src/lib/modules/fusionBookmarksModule/fusionBookmarksModule.ts
+++ packages/workspace-fusion-modules/src/lib/modules/fusionBookmarksModule/fusionBookmarksModule.ts
@@ -1,6 +1,7 @@
 import { FusionMediator, FusionWorkspaceModule } from '@equinor/workspace-fusion';
 import { ActionResponse, BookmarkActions } from './types/actions';
 import { bookmarksApi } from './utils/bookmarksApi';
+import { getFusionClient } from './utils/getClientAsync';
 import { checkForBookmarkId } from './utils/parseQueryParameters';
 import { post } from './utils/post';
 import { isBookmarkAction } from './utils/typeGuard';
@@ -18,13 +19,13 @@ async function setup(mediator: FusionMediator<unknown>, appKey: string) {
 	mediator.urlService.onUrlChange(checkForBookmarkId);
 	mediator.onMount(checkForBookmarkId);
 
-	const bookmarkHandler = (event: MessageEvent<unknown>) => handler(event, mediator);
+	const bookmarkHandler = (event: MessageEvent<unknown>) => handler(event, mediator, appKey);
 
 	mediator.onMount(() => window.addEventListener('message', bookmarkHandler));
 	mediator.onUnMount(() => window.removeEventListener('message', bookmarkHandler));
 }
 
-async function handler(event: MessageEvent<unknown>, mediator: FusionMediator<unknown>): Promise<void> {
+async function handler(event: MessageEvent<unknown>, mediator: FusionMediator<unknown>, appKey: string): Promise<void> {
 	if (!isBookmarkAction(event.data)) return;
 	const action = event.data;
 	switch (action.type) {
@@ -40,7 +41,7 @@ async function handler(event: MessageEvent<unknown>, mediator: FusionMediator<un
 
 		case 'save': {
 			try {
-				await handleSave(action.name, mediator);
+				await handleSave(action.name, mediator, appKey);
 				messageHandler.success(action);
 			} catch (e) {
 				messageHandler.failed(action);
@@ -63,18 +64,49 @@ async function handler(event: MessageEvent<unknown>, mediator: FusionMediator<un
  * Applies a bookmark to the workspace
  */
 async function handleApply(bookmarkId: string, mediator: FusionMediator<unknown>) {
-	console.log(`applying bookmark with id ${bookmarkId}`);
 	const bookmark = await bookmarksApi.getBookmarkById(bookmarkId);
-	console.log(bookmark);
+	//Not ready yet
+	// mediator.bookmarkService.apply(bookmark);
 }
 /**
  * Saves a bookmark from the workspace
  */
-async function handleSave(name: string, mediator: FusionMediator<unknown>) {
-	console.log(`Saving bookmark requested ${name}`, mediator.bookmarkService.capture());
+async function handleSave(name: string, mediator: FusionMediator<unknown>, appKey: string) {
+	const payload = mediator.bookmarkService.capture();
+
+	//Need sourceSystem here
+	const bookmarkPayload: BookmarkPayload = {
+		appKey,
+		name,
+		contextId: '???',
+		isShared: false,
+		payload,
+		description: '',
+		sourceSystem: {} as any,
+	};
+	const client = getFusionClient();
+
+	const response = await (
+		await client
+	).fetch('/bookmarksss', { body: JSON.stringify(bookmarkPayload), method: 'POST' });
+	if (!response.ok) throw new Error('Response not ok');
 }
 
 const messageHandler = {
 	failed: (action: BookmarkActions) => post<ActionResponse<BookmarkActions>>({ status: 'Failed', action }),
 	success: (action: BookmarkActions) => post<ActionResponse<BookmarkActions>>({ status: 'Success', action }),
 };
+
+interface BookmarkPayload {
+	name: string;
+	description: string;
+	isShared: boolean;
+	appKey: string;
+	contextId: string;
+	payload: unknown;
+	sourceSystem: {
+		identifier: string;
+		name: string;
+		subSystem: string;
+	};
+}
