54581a395cebec4fb0d9a6d45470befa31688123
Gustav-Eikaas <guei@equinor.com>
2022-09-21 10:18:34 +0200

    refactor

--- packages/workspace-fusion-modules/src/lib/modules/fusionBookmarksModule/fusionBookmarksModule.ts
+++ packages/workspace-fusion-modules/src/lib/modules/fusionBookmarksModule/fusionBookmarksModule.ts
@@ -1,7 +1,6 @@
 import { FusionMediator, FusionWorkspaceModule } from '@equinor/workspace-fusion';
-import { ActionResponse } from './types/actions';
+import { ActionResponse, BookmarkActions } from './types/actions';
 import { bookmarksApi } from './utils/bookmarksApi';
-import { getFusionClient } from './utils/getClientAsync';
 import { checkForBookmarkId } from './utils/parseQueryParameters';
 import { post } from './utils/post';
 import { isBookmarkAction } from './utils/typeGuard';
@@ -26,41 +25,56 @@ async function setup(mediator: FusionMediator<any>, appKey: string) {
 }
 
 async function handler(event: MessageEvent<any>, mediator: FusionMediator<any>): Promise<void> {
-	if (!isBookmarkAction(event.data)) {
-		return;
-	}
-	switch (event.data.type) {
+	if (!isBookmarkAction(event.data)) return;
+	const action = event.data;
+	switch (action.type) {
 		case 'apply': {
 			try {
-				await handleApply(event.data.bookmarkId, mediator);
-				post<ActionResponse>({ status: 'Success' });
+				await handleApply(action.bookmarkId, mediator);
+				messageHandler.success(action);
 			} catch (e) {
-				post<ActionResponse>({ status: 'Failed' });
+				messageHandler.failed(action);
 			}
 			break;
 		}
 
 		case 'save': {
 			try {
-				handleSave(event.data.name, mediator);
-				post<ActionResponse>({ status: 'Success' });
+				handleSave(action.name, mediator);
+				messageHandler.success(action);
 			} catch (e) {
-				post<ActionResponse>({ status: 'Failed' });
+				messageHandler.failed(action);
 			}
 			break;
 		}
 
+		case 'ping': {
+			messageHandler.success(action);
+			break;
+		}
+
 		default:
+			messageHandler.failed(action);
 			break;
 	}
 }
 
+/**
+ * Applies a bookmark to the workspace
+ */
 async function handleApply(bookmarkId: string, mediator: FusionMediator<any>) {
 	console.log(`applying bookmark with id ${bookmarkId}`);
 	const bookmark = await bookmarksApi.getBookmarkById(bookmarkId);
 	console.log(bookmark);
 }
-
+/**
+ * Saves a bookmark from the workspace
+ */
 async function handleSave(name: string, mediator: FusionMediator<any>) {
 	console.log(`Saving bookmark requested ${name}`, mediator.bookmarkService.capture());
 }
+
+const messageHandler = {
+	failed: (action: BookmarkActions) => post<ActionResponse<BookmarkActions>>({ status: 'Failed', action }),
+	success: (action: BookmarkActions) => post<ActionResponse<BookmarkActions>>({ status: 'Success', action }),
+};
--- packages/workspace-fusion-modules/src/lib/modules/fusionBookmarksModule/utils/handleBookmarksFromPortal.ts
+++ packages/workspace-fusion-modules/src/lib/modules/fusionBookmarksModule/utils/handleBookmarksFromPortal.ts
@@ -2,46 +2,79 @@ import { BookmarkActions } from '../types/actions';
 import { post } from './post';
 import { isStatus } from './typeGuard';
 
+const BOOKMARK_ACTION_TIMEOUT = 5000;
+const PING_TIMEOUT = 1;
+
 //Entry point for portal to use bookmarks in workspace
-export function handleBoomarksFromPortal(action: BookmarkActions) {
-	return new Promise((res, rej) => {
-		/** Curry reject so it removes the event listener */
-		const curriedReject = (reason?: string) => {
-			window.removeEventListener('message', handler);
-			rej(reason);
-		};
-		const timeoutId = handleTimeout(curriedReject);
-		/** Curry resolve so it removes the event listener */
-		const curriedResolve = (value: unknown) => {
-			clearTimeout(timeoutId);
-			window.removeEventListener('message', handler);
-			res(value);
-		};
+export const handleBoomarksFromPortal = async (action: BookmarkActions) => {
+	await pingPromise();
+	return await bookmarksPromise(action);
+};
 
-		//Curry handler to get a variable to send to promiseHandler
-		const handler = (ev: MessageEvent<unknown>) => promiseHandler(ev, curriedResolve);
+function pingPromise() {
+	return Promise.race([
+		timeoutPromise(PING_TIMEOUT, 'Failed to reach server'),
+		new Promise((res, rej) => {
+			const pingHandler = (ev: MessageEvent<any>) => {
+				if (!isStatus(ev.data)) return;
+				window.removeEventListener('message', pingHandler);
+				if (ev.data.status === 'Success') {
+					res(ev.data.status);
+				} else {
+					rej(ev.data.status);
+				}
+			};
 
-		//Listen for the message that is returned from the listener that listens to the current return function
-		window.addEventListener('message', handler);
-		/** Send message and wait for response */
-		return post<BookmarkActions>(action);
-	});
+			window.addEventListener('message', pingHandler);
+			post<BookmarkActions>({ type: 'ping' });
+		}),
+	]);
 }
 
 // eslint-disable-next-line consistent-return
+const bookmarksPromise = (action: BookmarkActions) =>
+	Promise.race([
+		timeoutPromise(BOOKMARK_ACTION_TIMEOUT, 'Action took too long'),
+		new Promise((res, rej) => {
+			const curriedResolve = (value: unknown) => {
+				window.removeEventListener('message', handler);
+				res(value);
+			};
+
+			const curriedReject = (reason?: string) => {
+				rej(reason);
+				window.removeEventListener('message', handler);
+			};
+
+			//Curry handler to get a variable to send to promiseHandler
+			const handler = (ev: MessageEvent<unknown>) => promiseHandler(ev, curriedResolve, curriedReject);
+
+			//Listen for the message that is returned from the listener that listens to the current return function
+			window.addEventListener('message', handler);
+			/** Send message and wait for response */
+			return post<BookmarkActions>(action);
+		}),
+	]);
+
+//Reject promise if stalling
+const timeoutPromise = (timeout: number, reason: string) =>
+	new Promise((_, rej) => setTimeout(() => rej(reason), timeout));
+
 /**
  * Handle promise with timeout and wait for event
  * @param ev - Message event
  * @param res - Promise resolver
  */
-const promiseHandler = (ev: MessageEvent<unknown>, res: (value: unknown) => void): void => {
+const promiseHandler = (
+	ev: MessageEvent<unknown>,
+	res: (value: unknown) => void,
+	rej: (reason?: string) => void
+): void => {
 	/**Dont reject if not status because multiple events can be fired */
-	if (isStatus(ev.data)) {
+	if (!isStatus(ev.data)) return;
+	if (ev.data.status === 'Success') {
 		res(ev.data);
+	} else {
+		rej('Failed');
 	}
 };
-
-//Reject promise if stalling
-function handleTimeout(rej: (reason?: string) => void) {
-	return setTimeout(() => rej('Took to long to respond'), 5000);
-}
