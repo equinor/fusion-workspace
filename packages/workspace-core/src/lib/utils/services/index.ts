28616d2408cb086694af2fc620f643af6c43666c
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2022-10-17 08:45:54 +0200

    URL service cleanup (#127)
    * chore(urlService): replace custom logic with native code
    
    * chore(url code): reduce parsing code

--- packages/workspace-core/src/lib/utils/services/index.ts
+++ packages/workspace-core/src/lib/utils/services/index.ts
@@ -3,3 +3,4 @@ export { createErrorService } from './errorService';
 export { createDataService } from './dataService';
 export { createSelectionService } from './selectionService';
 export { createBookmarksService } from './bookmarkService';
+export { createUrlService } from './urlService';
