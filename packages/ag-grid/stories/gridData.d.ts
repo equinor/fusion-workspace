c5ce8f2a9a765ca61e03b5b7d863c03f14a6b435
Gustav-Eikaas <Gustav-Eikaas@users.noreply.github.com>
2022-11-02 13:06:10 +0000

    Prettified Code!

--- packages/ag-grid/stories/gridData.d.ts
+++ packages/ag-grid/stories/gridData.d.ts
@@ -1,9 +1,9 @@
 export declare type User = {
-    userId: string;
-    username: string;
-    email: string;
-    avatar: string;
-    password: string;
-    birthdate: Date;
+	userId: string;
+	username: string;
+	email: string;
+	avatar: string;
+	password: string;
+	birthdate: Date;
 };
 export declare const USERS: User[];
