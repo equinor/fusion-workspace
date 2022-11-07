66910d67d59fba6e881a26b5c0ca3f1aba58e98d
Gustav-Eikaas <guei@equinor.com>
2022-11-07 12:52:16 +0100

    build: :construction_worker: please build

--- packages/power-bi/src/lib/utils/getSlicerDataAsync.ts
+++ packages/power-bi/src/lib/utils/getSlicerDataAsync.ts
@@ -3,7 +3,7 @@ import { VisualDescriptor } from 'powerbi-client';
 
 export const getSlicerDataAsync = async (slicer: VisualDescriptor): Promise<SlicerData> => {
 	const { data } = await slicer.exportData();
-	const filter = (await slicer.getFilters()).at(0);
+	const filter = (await slicer.getFilters())[0];
 
 	return { data, filter, slicer };
 };
