62908510885176a33e1bf555a24ced6b24f34ab0
Gustav-Eikaas <guei@equinor.com>
2022-11-08 15:11:37 +0100

    feat: :sparkles: update garden config for garden and fusion-workspace

--- packages/workspace-fusion/src/lib/types/configuration.ts
+++ packages/workspace-fusion/src/lib/types/configuration.ts
@@ -11,7 +11,7 @@ export type GardenConfig<
 	TCustomGroupByKeys extends Record<PropertyKey, unknown> = Record<PropertyKey, unknown>,
 	TCustomState extends Record<PropertyKey, unknown> = Record<PropertyKey, unknown>,
 	TContext extends Record<PropertyKey, unknown> = Record<PropertyKey, unknown>
-> = Omit<OriginalGardenConfig<TData, TCustomGroupByKeys, TCustomState, TContext>, 'data'>;
+> = Omit<OriginalGardenConfig<TData, TCustomGroupByKeys, TCustomState, TContext>, 'data' | 'getIdentifier'>;
 
 export type GridConfig<T> = {
 	columnDefinitions: [ColDef<T>, ...ColDef<T>[]];
