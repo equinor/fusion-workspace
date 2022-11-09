d6d29119744914770b7f7ac92ff9551740a4e09e
Gustav-Eikaas <guei@equinor.com>
2022-11-09 09:37:38 +0100

    feat: :sparkles: export integration types from workspace-fusion

---
+++ packages/workspace-fusion/src/lib/integrations/grid/grid.ts
@@ -0,0 +1,11 @@
+import {
+	ColDef,
+	ColumnState,
+	GetIdentifier,
+	GridConfig,
+	GridController,
+	GridOptions,
+	ICellRendererProps,
+} from '@equinor/workspace-ag-grid';
+
+export type { ColDef, ColumnState, GetIdentifier, GridConfig, GridController, GridOptions, ICellRendererProps };
