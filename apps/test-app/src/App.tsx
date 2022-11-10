994d09314871acc633cf4f968dee0ad63500e9f5
Gustav-Eikaas <guei@equinor.com>
2022-11-10 14:55:09 +0100

    chore: fix test app to match changes

--- apps/test-app/src/App.tsx
+++ apps/test-app/src/App.tsx
@@ -1,44 +1,6 @@
 import './App.css';
-import { Workspace, createFusionWorkspace } from '@equinor/workspace-fusion';
+import { createFusionWorkspace } from '@equinor/workspace-fusion';
 import { isSubGroup, getGardenItems } from '@equinor/workspace-fusion/garden';
-
-console.log(isSubGroup, getGardenItems);
-
-type S = {
-	id: string;
-	age: number;
-};
-
-function App() {
-	return (
-		<div className="App" style={{ height: '100vh' }}>
-			<Workspace controller={createWorkspace()} />
-		</div>
-	);
-}
-
-export default App;
-
-function createWorkspace() {
-	const controller = createFusionWorkspace<S>({ appKey: 'Handover', getIdentifier: (s) => s.id }, (s) =>
-		s
-			.addFilter({
-				filterGroups: [
-					{
-						name: 'id',
-						valueFormatter: (s) => s.id,
-						isQuickFilter: true,
-					},
-				],
-			})
-			.addGrid({ columnDefinitions: [{ field: 'id' }, { field: 'age' }] })
-			.addStatusBarItems((s) => [{ title: 'count', value: s.length }])
-			.addMiddleware((s) => (s.dataService.data = MOCK_DATA))
-	);
-
-	return controller;
-}
-
 const MOCK_DATA: S[] = [
 	{
 		id: '123',
@@ -105,3 +67,34 @@ const MOCK_DATA: S[] = [
 		age: 18,
 	},
 ];
+
+type S = {
+	id: string;
+	age: number;
+};
+
+function App() {
+	return (
+		<div className="App" style={{ height: '100vh' }}>
+			<Workspace />
+		</div>
+	);
+}
+
+export default App;
+
+const Workspace = createFusionWorkspace<S>({ appKey: 'Handover', getIdentifier: (s) => s.id }, (s) =>
+	s
+		.addFilter({
+			filterGroups: [
+				{
+					name: 'id',
+					valueFormatter: (s) => s.id,
+					isQuickFilter: true,
+				},
+			],
+		})
+		.addGrid({ columnDefinitions: [{ field: 'id' }, { field: 'age' }] })
+		.addStatusBarItems((s) => [{ title: 'count', value: s.length }])
+		.addMiddleware((s) => (s.dataService.data = MOCK_DATA))
+);
