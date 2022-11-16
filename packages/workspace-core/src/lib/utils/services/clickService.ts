aca8f3a5dec480092af395657ee196c390322016
Gustav-Eikaas <guei@equinor.com>
2022-11-16 10:47:28 +0100

    chore: hide destructors from service consumers

--- packages/workspace-core/src/lib/utils/services/clickService.ts
+++ packages/workspace-core/src/lib/utils/services/clickService.ts
@@ -1,6 +1,7 @@
 import { Subject } from 'rxjs';
+import { ServiceCtor } from '../../types/serviceCtor';
 
-export function createClickService<TOnClick extends Record<PropertyKey, unknown>>() {
+export function createClickService<TOnClick extends Record<PropertyKey, unknown>>(destroy: ServiceCtor) {
 	const clickSubject = new Subject<TOnClick>();
 	return {
 		click: (click: TOnClick) => clickSubject.next(click),
--- packages/workspace-core/src/lib/utils/services/errorService.ts
+++ packages/workspace-core/src/lib/utils/services/errorService.ts
@@ -1,7 +1,10 @@
 import { Subject } from 'rxjs';
+import { ServiceCtor } from '../../types/serviceCtor';
 
-export function createErrorService<TError extends Record<PropertyKey, unknown>>() {
+export function createErrorService<TError extends Record<PropertyKey, unknown>>(destroy: ServiceCtor) {
 	const errorSubject = new Subject<TError>();
+	destroy(() => errorSubject.complete());
+
 	return {
 		error: (click: TError) => errorSubject.next(click),
 		error$: errorSubject.asObservable(),
