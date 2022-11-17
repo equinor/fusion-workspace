5e7b32e08689ac36e236d5988fb63897906841af
Gustav-Eikaas <guei@equinor.com>
2022-11-17 15:32:05 +0100

    refactor: :recycle: refactor function

--- packages/workspace-fusion/src/lib/utils/optionsChanged/dataSourceOptionsChanged.ts/dataSourceChanged.ts
+++ packages/workspace-fusion/src/lib/utils/optionsChanged/dataSourceOptionsChanged.ts/dataSourceChanged.ts
@@ -1,5 +1,7 @@
 import { createFetchFunction } from '../../../utils/dataSource/createFetchFunction';
-import { WorkspaceConfiguration } from '../../../types';
+import { FusionMediator, WorkspaceConfiguration } from '../../../types';
+import { FetchController } from '@equinor/workspace-data-source';
+import { DataSourceConfig } from 'lib/integrations/data-source';
 
 /**
  * Refetch data if data source options changed
@@ -21,9 +23,17 @@ export function didDataSourceOptionsChange<
 	if (current.rawOptions.dataOptions?.getResponseAsync !== previous.rawOptions.dataOptions?.getResponseAsync) {
 		/** Only react if there is new dataoptions, removing dataoptions is not currently supported */
 		if (current.rawOptions.dataOptions) {
-			dataSourceController.setFetch(createFetchFunction(current.rawOptions.dataOptions, previous.mediator));
-			dataSourceController.reset();
-			dataSourceController.getDataAsync();
+			handleDataSourceChanged(dataSourceController, current.rawOptions.dataOptions, previous.mediator);
 		}
 	}
+}
+
+function handleDataSourceChanged<TData extends Record<PropertyKey, unknown>>(
+	dataSourceController: FetchController<TData>,
+	dataOptions: DataSourceConfig<TData>,
+	mediator: FusionMediator<TData>
+) {
+	dataSourceController.setFetch(createFetchFunction(dataOptions, mediator));
+	dataSourceController.reset();
+	dataSourceController.getDataAsync();
 }
