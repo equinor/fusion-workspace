1e958b6b3cd4a67c0933e76e119560ce3ec05dba
Gustav-Eikaas <guei@equinor.com>
2022-11-23 15:03:05 +0100

    chore: :lipstick: refactor styling

--- packages/ag-grid/src/lib/package/src/styles/gridStyles.css.ts
+++ packages/ag-grid/src/lib/package/src/styles/gridStyles.css.ts
@@ -1,72 +1,15 @@
-import {
-	aggregation,
-	arrows,
-	asc,
-	cancel,
-	chart,
-	colorPicker,
-	columns,
-	contracted,
-	copy,
-	cross,
-	csv,
-	desc,
-	excel,
-	expanded,
-	eye,
-	eyeSlash,
-	filter,
-	first,
-	grip,
-	group,
-	last,
-	left,
-	linked,
-	loading,
-	maximize,
-	menu,
-	minimize,
-	next,
-	none,
-	notAllowed,
-	paste,
-	pin,
-	pivot,
-	previous,
-	right,
-	save,
-	smallDown,
-	smallLeft,
-	smallRight,
-	smallUp,
-	tick,
-	treeClosed,
-	treeIndeterminate,
-	treeOpen,
-	unlinked,
-} from '../icons';
+import { agIcons } from './icons/agIcons.css';
+import { agVariables } from './variables.ts/variables.css';
 
 export const THEME_NAME = 'ag-theme-material';
 
-function parseIcon(value: string) {
-	return `data:image/svg+xml,${encodeURIComponent(value)}`;
-}
-
-function injectIcon(value: string) {
-	return `background: transparent
-  url(${parseIcon(value)})
-    center/contain no-repeat;
-  color: transparent;
-  height: 20px;
-  width: 20px;`;
-}
-
 export const agStyles = `
 .ag-theme-material {
+    ${agVariables}
     -webkit-font-smoothing: antialiased;
     color: rgba(0, 0, 0, 0.87);
     color: var(--ag-foreground-color, rgba(0, 0, 0, 0.87));
-    font-family: "Roboto", sans-serif;
+    font-family: Equinor, sans-serif;
     font-size: 13px;
     line-height: normal;
   }
@@ -88,150 +31,7 @@ export const agStyles = `
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-  .ag-theme-material .ag-icon-aggregation {
-    ${injectIcon(aggregation)}
-  }
-  .ag-theme-material .ag-icon-arrows {
-    ${injectIcon(arrows)}
-  }
-  .ag-theme-material .ag-icon-asc {
-    ${injectIcon(asc)}
-  }
-  .ag-theme-material .ag-icon-cancel {
-    ${injectIcon(cancel)}
-  }
-  .ag-theme-material .ag-icon-chart {
-    ${injectIcon(chart)}
-  }
-  .ag-theme-material .ag-icon-color-picker {
-    ${injectIcon(colorPicker)}
-  }
-  .ag-theme-material .ag-icon-columns {
-    ${injectIcon(columns)}
-  }
-  .ag-theme-material .ag-icon-contracted {
-    ${injectIcon(contracted)}
-  }
-  .ag-theme-material .ag-icon-copy {
-    ${injectIcon(copy)}
-  }
-  .ag-theme-material .ag-icon-cross {
-    ${injectIcon(cross)}
-  }
-  .ag-theme-material .ag-icon-csv {
-    ${injectIcon(csv)}
-  }
-  .ag-theme-material .ag-icon-desc {
-    ${injectIcon(desc)}
-  }
-  .ag-theme-material .ag-icon-excel {
-    ${injectIcon(excel)}
-  }
-  .ag-theme-material .ag-icon-expanded {
-    ${injectIcon(expanded)}
-  }
-  .ag-theme-material .ag-icon-eye-slash {
-    ${injectIcon(eyeSlash)}
-  }
-  .ag-theme-material .ag-icon-eye {
-    ${injectIcon(eye)}
-  }
-  .ag-theme-material .ag-icon-filter {
-    ${injectIcon(filter)}
-  }
-  .ag-theme-material .ag-icon-first {
-    ${injectIcon(first)}
-  }
-  .ag-theme-material .ag-icon-grip {
-    ${injectIcon(grip)}
-  }
-  .ag-theme-material .ag-icon-group {
-    ${injectIcon(group)}
-  }
-  .ag-theme-material .ag-icon-last {
-    ${injectIcon(last)}
-  }
-  .ag-theme-material .ag-icon-left {
-    ${injectIcon(left)}
-  }
-  .ag-theme-material .ag-icon-linked {
-    ${injectIcon(linked)}
-  }
-  .ag-theme-material .ag-icon-loading {
-    ${injectIcon(loading)}
-  }
-  .ag-theme-material .ag-icon-maximize {
-    ${injectIcon(maximize)}
-  }
-  .ag-theme-material .ag-icon-menu {
-   ${injectIcon(menu)}
-  }
-  .ag-theme-material .ag-icon-minimize {
-    ${injectIcon(minimize)}
-  }
-  .ag-theme-material .ag-icon-next {
-    ${injectIcon(next)}
-  }
-  .ag-theme-material .ag-icon-none {
-    ${injectIcon(none)}
-  }
-  .ag-theme-material .ag-icon-not-allowed {
-    ${injectIcon(notAllowed)}
-  }
-  .ag-theme-material .ag-icon-paste {
-    ${injectIcon(paste)}
-  }
-  .ag-theme-material .ag-icon-pin {
-    ${injectIcon(pin)}
-  }
-  .ag-theme-material .ag-icon-pivot {
-    ${injectIcon(pivot)}
-  }
-  .ag-theme-material .ag-icon-previous {
-    ${injectIcon(previous)}
-  }
-  .ag-theme-material .ag-icon-right {
-    ${injectIcon(right)}
-  }
-  .ag-theme-material .ag-icon-save {
-    ${injectIcon(save)}
-  }
-  .ag-theme-material .ag-icon-small-down {
-    ${injectIcon(smallDown)}
-  }
-  .ag-theme-material .ag-icon-small-left {
-    ${injectIcon(smallLeft)}
-  }
-  .ag-theme-material .ag-icon-small-right {
-    ${injectIcon(smallRight)}
-  }
-  .ag-theme-material .ag-icon-small-up {
-    ${injectIcon(smallUp)}
-  }
-  .ag-theme-material .ag-icon-tick {
-    ${injectIcon(tick)}
-  }
-  .ag-theme-material .ag-icon-tree-closed {
-    ${injectIcon(treeClosed)}
-  }
-  .ag-theme-material .ag-icon-tree-indeterminate {
-    ${injectIcon(treeIndeterminate)}
-  }
-  .ag-theme-material .ag-icon-tree-open {
-    ${injectIcon(treeOpen)}
-  }
-  .ag-theme-material .ag-icon-unlinked {
-    ${injectIcon(unlinked)}
-  }
-  .ag-theme-material .ag-icon-row-drag::before {
-    content: "\f116";
-  }
-  .ag-theme-material .ag-left-arrow::before {
-    ${injectIcon(left)}
-  }
-  .ag-theme-material .ag-right-arrow::before {
-    ${injectIcon(right)}
-  }
+  ${agIcons}
   .ag-theme-material .ag-root-wrapper, .ag-theme-material .ag-sticky-top {
     background-color: #fff;
     background-color: var(--ag-background-color, #fff);
@@ -4209,7 +4009,7 @@ export const agStyles = `
   .ag-theme-material .ag-header {
     background-color: #fff;
     background-color: var(--ag-header-background-color, #fff);
-    border-bottom: solid 1px;
+    border-bottom: solid 2px;
     border-bottom-color: #e2e2e2;
     border-bottom-color: var(--ag-border-color, #e2e2e2);
   }
@@ -4446,6 +4246,7 @@ export const agStyles = `
   }
   .ag-theme-material .ag-column-drop-cell-text {
     margin: 0 8px;
+
   }
   .ag-theme-material .ag-column-drop-cell-button {
     min-width: 32px;
---
+++ packages/ag-grid/src/lib/package/src/styles/variables.ts/variables.css.ts
@@ -0,0 +1,80 @@
+import { tokens } from '@equinor/eds-tokens';
+import { concat } from '../../utils/concat';
+
+const unassigned = 'grey';
+
+/**input */
+const inputVariables = `
+--ag-input-focus-border-color: ${unassigned};
+--ag-input-border-color-invalid: ${unassigned};
+--ag-input-focus-border-color: ${unassigned};
+--ag-input-disabled-background-color: ${unassigned};
+--ag-input-disabled-border-color: ${unassigned};
+`;
+
+//--ag-line-height: ${unassigned};
+/**
+--ag-secondary-foreground-color: ${unassigned};
+--ag-data-color: ${unassigned};
+--ag-background-color: ${unassigned};
+--ag-disabled-foreground-color: ${unassigned};
+ */
+const general = `
+--ag-border-color: ${tokens.colors.ui.background__medium.hex};
+--ag-foreground-color: ${tokens.colors.text.static_icons__default.hex};
+--ag-material-primary-color: ${tokens.colors.interactive.primary__resting.hex};
+
+`;
+/**
+ --ag-row-hover-color: ${unassigned};
+--ag-row-border-color: ${unassigned};
+--ag-selected-row-background-color: ${unassigned};
+--ag-odd-row-background-color: ${unassigned};
+ */
+/** General */
+const row = `
+--ag-row-hover-color: ${tokens.colors.ui.background__medium.hex};
+--ag-row-border-color: ${tokens.colors.ui.background__medium.hex};
+`;
+
+/** Row */
+
+/** Column */
+const column = `
+--ag-column-hover-color: ${unassigned};
+`;
+
+/** Header */
+const header = `
+--ag-subheader-background-color: ${tokens.colors.interactive.table__header__fill_resting.hex};
+--ag-header-background-color: ${tokens.colors.interactive.table__header__fill_resting.hex};
+`;
+
+const controlPanel = `
+--ag-control-panel-background-color: ${unassigned};
+`;
+
+/**
+ --ag-range-selection-highlight-color: ${unassigned};
+--ag-range-selection-border-color: ${unassigned};
+--ag-range-selection-background-color-1: ${unassigned};
+--ag-selected-tab-underline-color: ${unassigned};
+ */
+
+/** Selection */
+const selection = `
+--ag-selected-row-background-color: ${tokens.colors.interactive.table__cell__fill_activated.hex};
+`;
+
+/** Value change */
+const valueChange = `
+--ag-value-change-delta-up-color: ${unassigned};
+--ag-value-change-delta-down-color: ${unassigned};
+--ag-value-change-value-highlight-background-color: ${unassigned};
+`;
+
+const checkbox = `
+--ag-checkbox-unchecked-color: red;
+`;
+
+export const agVariables = concat(general, header, row, selection, checkbox);
