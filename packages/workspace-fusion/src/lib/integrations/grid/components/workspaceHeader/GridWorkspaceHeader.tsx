b8bc3a21083bb1861db7a728778779c4a364de9c
Gustav-Eikaas <guei@equinor.com>
2022-11-30 08:22:52 +0100

    refactor: :recycle: extract divider to common in workspace-fusion

--- packages/workspace-fusion/src/lib/integrations/grid/components/workspaceHeader/GridWorkspaceHeader.tsx
+++ packages/workspace-fusion/src/lib/integrations/grid/components/workspaceHeader/GridWorkspaceHeader.tsx
@@ -1,21 +1,9 @@
-import { Icon } from '@equinor/eds-core-react';
-import { tokens } from '@equinor/eds-tokens';
 import { GridController } from '@equinor/workspace-ag-grid';
-import { TabButton } from '../../../../components/TabNavigation/tabNavigation.styles';
-import styled from 'styled-components';
 import { WorkspaceHeader } from '../../../../components/Header/WorkspaceHeader';
 
 type GridHeaderProps<TData extends Record<PropertyKey, unknown>> = {
 	controller: GridController<TData>;
 };
-
-const StyledDivider = styled.hr`
-	width: 1px;
-	height: auto;
-	align-self: stretch;
-	background: ${tokens.colors.ui.background__medium.hex};
-	border: none;
-`;
 
 export function GridHeader<TData extends Record<PropertyKey, unknown>>({ controller }: GridHeaderProps<TData>) {
 	return (
