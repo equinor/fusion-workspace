e40aecec6b4ad205df74d97042c4bc1a140b14a1
Gustav-Eikaas <guei@equinor.com>
2022-11-30 12:54:41 +0100

    chore: fix build error

--- packages/workspace-fusion/src/lib/integrations/status-bar/types/workspaceProps.ts
+++ packages/workspace-fusion/src/lib/integrations/status-bar/types/workspaceProps.ts
@@ -1,4 +1,4 @@
-import { StatusBarConfig } from '../status-bar';
+import { StatusBarConfig } from './workspaceConfig';
 
 export type WorkspaceStatusBarProps<TData extends Record<PropertyKey, unknown>> = {
 	statusBarOptions?: StatusBarConfig<TData>;
