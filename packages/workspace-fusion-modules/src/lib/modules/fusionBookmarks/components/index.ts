fc94a98215d7c2fda86cea91ff3bebfcf3e7a81d
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2022-12-13 16:05:04 +0100

    feat: :sparkles: Load bookmarks from url (#257)
    * feat: :sparkles: bookmarks icon from fusion
    
    * refactor: :recycle: cr fixes
    
    * chore: add tsdoc
    
    * chore: remove unused deps
    
    * chore: export useful components
    
    * chore: add left right icons to header
    
    * refactor: :recycle: rename files
    
    * chore: reuse component to ensure consistent styling
    
    * chore: improve provider system in workspace react
    
    * chore: :fire: remove unused
    
    * chore: :fire: dead imports
    
    * refactor: :recycle: cr fixes
    
    * chore: cr fixes

---
+++ packages/workspace-fusion-modules/src/lib/modules/fusionBookmarks/components/index.ts
@@ -0,0 +1,2 @@
+export * from './HeaderIcon';
+export * from './BookmarkLoader';
--- packages/workspace-fusion/src/index.ts
+++ packages/workspace-fusion/src/index.ts
@@ -1,3 +1,5 @@
 import { Workspace } from './lib';
 export * from './lib';
+import { WorkspaceViewController } from '@equinor/workspace-react';
+export { WorkspaceViewController };
 export default Workspace;
