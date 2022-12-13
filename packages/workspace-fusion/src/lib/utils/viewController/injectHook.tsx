a1eca24830c9b1f79221d29a5dfe757cb644a86e
Gustav-Eikaas <guei@equinor.com>
2022-12-13 15:11:03 +0100

    chore: prevent remounting

--- packages/workspace-fusion/src/lib/utils/viewController/injectHook.tsx
+++ packages/workspace-fusion/src/lib/utils/viewController/injectHook.tsx
@@ -9,6 +9,8 @@ type InjectHookArgs = {
  * Injects a hook into workspace
  */
 export const injectHook = ({ effects, name, viewController }: InjectHookArgs) => {
+	/** Prevent remounting of hooks/providers */
+	if (!!viewController.providers.find((s) => s.name === name)) return;
 	viewController.addProvider({
 		name,
 		Component: ({ children }) => {
