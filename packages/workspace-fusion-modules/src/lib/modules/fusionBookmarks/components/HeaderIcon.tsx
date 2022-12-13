cfc3cfa38b1a32e6554e7d370b701c3496862c6b
Gustav-Eikaas <guei@equinor.com>
2022-12-13 13:27:12 +0100

    chore: reuse component to ensure consistent styling

--- packages/workspace-fusion-modules/src/lib/modules/fusionBookmarks/components/HeaderIcon.tsx
+++ packages/workspace-fusion-modules/src/lib/modules/fusionBookmarks/components/HeaderIcon.tsx
@@ -1,22 +1,14 @@
 import { Icon } from '@equinor/eds-core-react';
 import { bookmarks } from '@equinor/eds-icons';
 import { tokens } from '@equinor/eds-tokens';
+import { TabButton } from '@equinor/workspace-fusion';
 
 Icon.add({ bookmarks });
 
 export const HeaderIcon = () => {
 	return (
-		<div
-			style={{
-				height: '48px',
-				width: '48px',
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				cursor: 'pointer',
-			}}
-		>
+		<TabButton isActive={false}>
 			<Icon name="bookmarks" color={tokens.colors.interactive.primary__resting.hex} />
-		</div>
+		</TabButton>
 	);
 };
