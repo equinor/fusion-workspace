80c41ac242548be78ccf8cc1540fabfb7d9b36a1
Gustav Eikaas <guei@equinor.com>
2023-01-27 08:13:24 +0100

    chore: bump test-app

--- apps/test-app/src/main.tsx
+++ apps/test-app/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={client}>
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
