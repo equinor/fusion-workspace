5ad8f48701221c991d014d050f3d3435aaed73be
Gustav Eikaas <guei@equinor.com>
2023-01-30 09:11:58 +0100

    docs: :memo: improve tsdoc

--- packages/workspace-fusion/src/lib/integrations/data-source/types/index.ts
+++ packages/workspace-fusion/src/lib/integrations/data-source/types/index.ts
@@ -5,12 +5,27 @@ export type FetchData<TData> = (signal?: AbortSignal) => TData[] | Promise<TData
 export type { DataSourceConfig };
 
 type DataSourceConfig<TData> = {
-	/** Used to invalidate query */
+	/** Used to control query invalidation.
+	 *
+	 * Defaults to ["Workspace"]
+	 *
+	 * ```TS
+	 * 	const contextId = useContext();
+	 * <Workspace
+	 * {...}
+	 * //Will refetch data when contextId changes
+	 * dataSource={{queryKey: ["Workspace", contextId ]}}
+	 * />
+	 * ```
+	 */
 	queryKey?: string[];
 
 	initialData?: TData[];
 
-	/** Function for getting response object from server */
+	/**
+	 *  Function for getting response object from server
+	 *  Implementation will check response code and throw error if response code is not OK.
+	 */
 	getResponseAsync: (signal?: AbortSignal) => Promise<Response>;
 	/**
 	 * Function for parsing response
