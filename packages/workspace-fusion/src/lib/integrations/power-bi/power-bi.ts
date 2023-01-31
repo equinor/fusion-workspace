b808d754b1a7375538261b4e411a1594fe37c1a3
Gustav Eikaas <guei@equinor.com>
2023-01-31 12:30:59 +0100

    docs: :memo: add tsdoc to getErrorMessage

--- packages/workspace-fusion/src/lib/integrations/power-bi/power-bi.ts
+++ packages/workspace-fusion/src/lib/integrations/power-bi/power-bi.ts
@@ -58,6 +58,16 @@ type PowerBiConfig = {
 	reportUri: string;
 	getEmbed: (reportUri: string, token: string, signal?: AbortSignal) => Promise<EmbedConfiguration>;
 	getToken: (reportUri: string, signal?: AbortSignal) => Promise<FusionPowerBiToken>;
+	/**
+	 * To trigger getErrorMessage, throw error with response as cause in either getEmbed or getToken
+	 * @example
+	 * ```TS
+	 * const res = await getEmbed();
+	 * if(!res.ok){
+	 * throw new Error("anything", {cause: res})
+	 * }
+	 * ```
+	 */
 	getErrorMessage: (reportUri: string, signal?: AbortSignal) => Promise<string>;
 	filters?: FilterConfig;
 };
