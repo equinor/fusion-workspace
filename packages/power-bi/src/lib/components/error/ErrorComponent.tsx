7b9207cab80d301a9c7cef5a6fb11673225f24e2
Gustav Eikaas <guei@equinor.com>
2023-02-01 08:31:11 +0100

    chore: change to instanceof

--- packages/power-bi/src/lib/components/error/ErrorComponent.tsx
+++ packages/power-bi/src/lib/components/error/ErrorComponent.tsx
@@ -12,7 +12,7 @@ type ErrorComponentProps = {
 export function ErrorComponent({ getErrorMessage, reportUri, error }: ErrorComponentProps) {
 	return (
 		<StyledLoadingWrapper>
-			{isResponse(error.cause) && unauthCodes.includes(error.cause.status) ? (
+			{error.cause instanceof Response && unauthCodes.includes(error.cause.status) ? (
 				<ReportErrorMessage getErrorMessage={getErrorMessage} reportUri={reportUri} />
 			) : (
 				<div>Failed to load report</div>
@@ -20,11 +20,3 @@ export function ErrorComponent({ getErrorMessage, reportUri, error }: ErrorCompo
 		</StyledLoadingWrapper>
 	);
 }
-
-const keys: (keyof Response)[] = ['body', 'json', 'status', 'url', 'type', 'statusText', 'headers'];
-const isResponse = (d: unknown): d is Response => {
-	if (typeof d === 'object' && d !== null) {
-		return Object.keys(d).every((s) => (keys as string[]).includes(s));
-	}
-	return false;
-};
