3e55119cd33df24a81cbabc0f5e4ebad0e18666c
Gustav Eikaas <guei@equinor.com>
2023-02-03 15:05:56 +0100

    docs: :memo: added astro docs

---
+++ documentation/src/consts.ts
@@ -0,0 +1,40 @@
+export const SITE = {
+	title: 'Documentation',
+	description: 'Fusion workspace documentation.',
+	defaultLanguage: 'en-us',
+} as const;
+
+export const OPEN_GRAPH = {
+	image: {
+		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
+		alt:
+			'astro logo on a starry expanse of space,' +
+			' with a purple saturn-like planet floating in the right foreground',
+	},
+	twitter: 'astrodotbuild',
+};
+
+// See "Algolia" section of the README for more information.
+export const ALGOLIA = {
+	indexName: 'XXXXXXXXXX',
+	appId: 'XXXXXXXXXX',
+	apiKey: 'XXXXXXXXXX',
+};
+
+export type Sidebar = Record<string, { text: string; link: string }[]>;
+export const SIDEBAR: Sidebar = {
+	Configuration: [
+		{ text: 'Introduction', link: 'introduction' },
+		{ text: 'Data', link: 'config/data-options' },
+		{ text: 'Grid', link: 'config/grid-options' },
+		{ text: 'Garden', link: 'config/garden-options' },
+		{ text: 'PowerBi', link: 'config/powerbi-options' },
+		{ text: 'Filter', link: 'config/filter-options' },
+		{ text: 'Context', link: 'config/context-options' },
+	],
+	Examples: [
+		{ text: 'PowerBi only', link: 'examples/power-bi-only' },
+		{ text: 'Simple', link: 'examples/simple' },
+		{ text: 'Advanced', link: 'examples/advanced' },
+	],
+};
