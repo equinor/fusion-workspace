9158566a7c28a55477f03f5e8eb15e12f448a668
Gustav Eikaas <guei@equinor.com>
2023-02-06 10:04:03 +0100

    build: externalize react (#2)

--- packages/workspace-fusion/vite.config.ts
+++ packages/workspace-fusion/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import resolve from 'vite-plugin-resolve';
+import { react } from 'vite-plugin-resolve/presets';
 import bundleAnalyzer from 'rollup-plugin-bundle-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [],
 	appType: 'custom',
+
 	build: {
 		outDir: 'dist',
 		lib: {
@@ -15,15 +17,7 @@ export default defineConfig({
 			name: 'workspace',
 		},
 		rollupOptions: {
-			external: ['react', 'react-dom', 'react/jsx-runtime'],
-			//uncomment to debug
-			// plugins: [bundleAnalyzer({})],
-			output: {
-				globals: {
-					react: 'React',
-					'react-dom': 'ReactDOM',
-				},
-			},
+			external: (id) => /^react|styled-jsx/.test(id),
 		},
 	},
 	server: {
