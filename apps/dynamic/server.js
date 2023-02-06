3c8ebf0d469ab8a7afbd84f25465423de20f73ba
Gustav-Eikaas <Gustav-Eikaas@users.noreply.github.com>
2023-02-06 11:41:59 +0000

    Prettified Code!

--- apps/dynamic/server.js
+++ apps/dynamic/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
-const cors = require("cors")
+const cors = require('cors');
 
 const app = express();
-app.use(cors({origin: true}))
+app.use(cors({ origin: true }));
 
 app.get('/', (req, res, ctx) => {
-  res.sendFile(path.join(__dirname, "../../packages/workspace-fusion/dist/workspace.js"))
+	res.sendFile(path.join(__dirname, '../../packages/workspace-fusion/dist/workspace.js'));
 });
-
 
 app.listen(5000, () => console.log('Example app is listening on port 3000.'));
