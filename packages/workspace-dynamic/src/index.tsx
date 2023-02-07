6910db29c5ef1ecae7b19cc957548771511a5e4b
Gustav Eikaas <guei@equinor.com>
2023-02-07 13:47:58 +0100

    fix: :bug: add queryClient provider

--- packages/workspace-dynamic/src/index.tsx
+++ packages/workspace-dynamic/src/index.tsx
@@ -5,10 +5,13 @@ import { CircularProgress } from '@equinor/eds-core-react';
 import { Center } from './components/Center';
 import { useVersionOverride } from './hooks/useVersionOverride';
 import { InnerLoader } from './components/InnerLoader';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 //danger bundle
 import type { WorkspaceVitePlugin } from '@equinor/workspace-fusion';
 export { WorkspaceVitePlugin };
+
+const client = new QueryClient();
 
 export const Workspace = <
 	TData extends Record<PropertyKey, unknown>,
@@ -22,18 +25,20 @@ export const Workspace = <
 	const loadPath = useVersionOverride();
 
 	return (
-		<ErrorBoundary resetKeys={[loadPath]} FallbackComponent={() => <Center>Failed to load workspace</Center>}>
-			<Suspense
-				fallback={
-					<Center>
-						<CircularProgress size={48} />
-						<div>Loading workspace...</div>
-					</Center>
-				}
-			>
-				<InnerLoader loadPath={loadPath} {...props} />
-			</Suspense>
-		</ErrorBoundary>
+		<QueryClientProvider client={client}>
+			<ErrorBoundary resetKeys={[loadPath]} FallbackComponent={() => <Center>Failed to load workspace</Center>}>
+				<Suspense
+					fallback={
+						<Center>
+							<CircularProgress size={48} />
+							<div>Loading workspace...</div>
+						</Center>
+					}
+				>
+					<InnerLoader loadPath={loadPath} {...props} />
+				</Suspense>
+			</ErrorBoundary>
+		</QueryClientProvider>
 	);
 };
 
