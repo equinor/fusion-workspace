3c305a0b6a3d801b35710f6f9487c72b2bbcef61
Magnus Rode Gjøsund <54101675+MagnusGjosund@users.noreply.github.com>
2023-03-03 08:03:22 +0100

    chore: add modules documentation (#345)
    * chore: add modules documentation
    
    * small fix

--- documentation/src/consts.ts
+++ documentation/src/consts.ts
@@ -30,6 +30,7 @@ export const SIDEBAR: Sidebar = {
     { text: 'PowerBi', link: `config/powerbi-options` },
     { text: 'Filter', link: `config/filter-options` },
     { text: 'Context', link: `config/context-options` },
+    { text: 'Modules', link: `config/modules-options` },
   ],
   Examples: [
     { text: 'PowerBi only', link: `examples/power-bi-only` },
