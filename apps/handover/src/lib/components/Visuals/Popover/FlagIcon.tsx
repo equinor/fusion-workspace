9a52d66d0ee9961e856e552e4c66ed56b5fe7adc
Gustav Eikaas <guei@equinor.com>
2023-03-24 12:04:26 +0100

    chore: :fire: delete handover

--- apps/handover/src/lib/components/Visuals/Popover/FlagIcon.tsx
+++
@@ -1,5 +0,0 @@
-export const FlagIcon = ({ color }: { color: string }): JSX.Element => (
-  <svg width="12" height="14" viewBox="0 0 12 14" xmlns="http://www.w3.org/2000/svg">
-    <path d="M7.52 1.647 7.2 0H0v14h1.6V8.235h4.48l.32 1.647H12V1.647H7.52Z" fill={color} />
-  </svg>
-);
--- apps/handover/src/lib/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { FusionApp } from './FusionApp';
