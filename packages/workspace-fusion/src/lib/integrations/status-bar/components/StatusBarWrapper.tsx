8717125108f011a818e919a945089d5cfe3a5511
Gustav Eikaas <guei@equinor.com>
2023-04-25 15:55:29 +0200

    style: :lipstick: add loading skeletons to status bar

--- packages/workspace-fusion/src/lib/integrations/status-bar/components/StatusBarWrapper.tsx
+++ packages/workspace-fusion/src/lib/integrations/status-bar/components/StatusBarWrapper.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import { BaseEvent } from '@equinor/workspace-core';
 import { useQuery } from '@tanstack/react-query';
 import { StatusBarConfig } from '../types/workspaceConfig';
@@ -26,7 +27,39 @@ export function StatusBarWrapper<
   });
 
   if (isLoading || !data) {
-    return <div></div>;
+    return (
+      <StyledSkeletons>
+        {new Array(4).fill(0).map((_, i) => (
+          <SkeletonPackage height={38} width={90} key={i} />
+        ))}
+      </StyledSkeletons>
+    );
   }
   return <StatusBar items={data} />;
 }
+
+const StyledSkeletons = styled.div`
+  height: 48px;
+  display: flex;
+  gap: 30px;
+  width: 100%;
+  flex-direction: row;
+  margin-left: 24px;
+`;
+
+export const SkeletonPackage = styled.div<{ height: number; width: number }>`
+  width: ${({ width }) => `${width}px`};
+  height: ${({ height }) => `${height}px`};
+  cursor: progress;
+  background: linear-gradient(0.25turn, transparent, #fff, transparent), linear-gradient(#eee, #eee);
+  background-repeat: no-repeat;
+  background-size: 315px 250px, 315px 180px, 100px 100px, 225px 30px;
+  background-position: -315px 0, 0 0, 0px 190px, 50px 195px;
+  border-radius: 5px;
+  animation: loading 1.5s infinite;
+  @keyframes loading {
+    to {
+      background-position: 315px 0, 0 0, 0 190px, 50px 195px;
+    }
+  }
+`;
