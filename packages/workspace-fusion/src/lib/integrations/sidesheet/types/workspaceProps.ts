bd44aebd8822c1173685a33336136cea7fa59def
Gustav Eikaas <guei@equinor.com>
2023-06-20 10:44:48 +0200

    docs: :memo: add tsdoc

--- packages/workspace-fusion/src/lib/integrations/sidesheet/types/workspaceProps.ts
+++ packages/workspace-fusion/src/lib/integrations/sidesheet/types/workspaceProps.ts
@@ -2,13 +2,13 @@ export type WorkspaceSidesheetProps<TData extends Record<PropertyKey, unknown>> 
   sidesheetOptions?: SidesheetConfig<TData>;
 };
 
-export type SidesheetConfig<TData> =
-  | { type: 'custom'; Component: () => JSX.Element }
-  | {
-      type: 'default';
-      DetailsSidesheet: (props: DetailsSidesheetProps<TData>) => JSX.Element;
-      CreateSidesheet?: () => JSX.Element;
-    };
+export type SidesheetConfig<TData> = {
+  type: 'default';
+  DetailsSidesheet: (props: DetailsSidesheetProps<TData>) => JSX.Element;
+  CreateSidesheet?: () => JSX.Element;
+};
+//Future for Piping & release control, disabled for now
+// | { type: 'custom'; Component: () => JSX.Element }
 
 export type DetailsSidesheetProps<TData> = {
   id: string;
