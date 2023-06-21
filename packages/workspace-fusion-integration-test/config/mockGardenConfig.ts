002e452fae975df3c349f1ab8ee5cf84199b3981
Gustav Eikaas <guei@equinor.com>
2023-06-21 12:20:58 +0200

    test: :white_check_mark: add query param tab test

---
+++ packages/workspace-fusion-integration-test/config/mockGardenConfig.ts
@@ -0,0 +1,42 @@
+import { vi } from 'vitest';
+import { gardenModule } from '@equinor/workspace-fusion/garden-module';
+
+export const getMockGardenConfig = () => {
+  const fakeGardenMeta = vi.fn();
+  const fakeBlock = vi.fn();
+  const fakeHeaders = vi.fn();
+
+  const gardenConfig = {
+    getSubgroupItems: () => {
+      throw new Error();
+    },
+    getBlockAsync: async () => {
+      fakeBlock();
+      return [{ columnName: '123', items: [], subGroupCount: 0, subGroups: [], totalItemsCount: 0 }];
+    },
+    getDisplayName: () => '',
+    getGardenMeta: async () => {
+      fakeGardenMeta();
+      return {
+        allGroupingOptions: [],
+        columnCount: 2,
+        columnStart: 0,
+        rowCount: 1,
+        validGroupingOptions: [],
+      };
+    },
+    getHeader: async () => {
+      fakeHeaders();
+      return [{ count: 1, name: '123' }];
+    },
+    initialGrouping: ['finalizing'],
+  };
+
+  return {
+    gardenConfig,
+    fakeBlock,
+    fakeGardenMeta,
+    fakeHeaders,
+    gardenModule,
+  };
+};
--- packages/workspace-fusion-integration-test/tests/Garden.test.tsx
+++ packages/workspace-fusion-integration-test/tests/Garden.test.tsx
@@ -1,48 +1,16 @@
 import React from 'react';
 import { Workspace } from '@equinor/workspace-fusion';
-import { gardenModule } from '@equinor/workspace-fusion/garden-module';
 import { act, render } from '@testing-library/react';
-import { vi } from 'vitest';
 import { workspaceOptions } from '../config/workspaceOptions';
 import { fakeTimeout } from '../utils/fakeTimeout';
+import { getMockGardenConfig } from '../config/mockGardenConfig';
 
 describe('Testing garden integration', () => {
   it('Should call all garden api functions', async () => {
-    const fakeGardenMeta = vi.fn();
-    const fakeBlock = vi.fn();
-    const fakeHeaders = vi.fn();
+    const { gardenModule, fakeGardenMeta, fakeHeaders, gardenConfig } = getMockGardenConfig();
+
     act(() => {
-      render(
-        <Workspace
-          workspaceOptions={workspaceOptions}
-          modules={[gardenModule]}
-          gardenOptions={{
-            getSubgroupItems: () => {
-              throw new Error();
-            },
-            getBlockAsync: async () => {
-              fakeBlock();
-              return [{ columnName: '123', items: [], subGroupCount: 0, subGroups: [], totalItemsCount: 0 }];
-            },
-            getDisplayName: () => '',
-            getGardenMeta: async () => {
-              fakeGardenMeta();
-              return {
-                allGroupingOptions: [],
-                columnCount: 2,
-                columnStart: 0,
-                rowCount: 1,
-                validGroupingOptions: [],
-              };
-            },
-            getHeader: async () => {
-              fakeHeaders();
-              return [{ count: 1, name: '123' }];
-            },
-            initialGrouping: ['finalizing'],
-          }}
-        />
-      );
+      render(<Workspace workspaceOptions={workspaceOptions} modules={[gardenModule]} gardenOptions={gardenConfig} />);
     });
 
     //Have to make an artificial timeout to ensure all functions are called.
