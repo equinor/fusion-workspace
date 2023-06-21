3720946b7dcc08c15e9e823361fd2eca196bf238
Gustav Eikaas <guei@equinor.com>
2023-06-21 10:52:27 +0200

    chore: finding a safe timeout

--- packages/workspace-fusion-integration-test/utils/fakeTimeout.ts
+++ packages/workspace-fusion-integration-test/utils/fakeTimeout.ts
@@ -1,1 +1,1 @@
-export const fakeTimeout = (timeout?: number) => new Promise<void>((res) => setTimeout(() => res(), timeout ?? 1));
+export const fakeTimeout = (timeout?: number) => new Promise<void>((res) => setTimeout(() => res(), timeout ?? 100));
