f17d7ce6ad648305f21fa2c5f85723c0f945809c
Gustav Eikaas <guei@equinor.com>
2023-06-23 12:27:32 +0200

    test: :test_tube: add more tests for garden component

--- packages/workspace-fusion-integration-test/tests/Garden.test.tsx
+++ packages/workspace-fusion-integration-test/tests/Garden.test.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Workspace } from '@equinor/workspace-fusion';
-import { act, render } from '@testing-library/react';
+import { act, render, getByText } from '@testing-library/react';
 import { workspaceOptions } from '../config/workspaceOptions';
 import { fakeTimeout } from '../utils/fakeTimeout';
 import { getMockGardenConfig } from '../config/mockGardenConfig';
@@ -18,5 +18,92 @@ describe('Testing garden integration', () => {
 
     expect(fakeGardenMeta).toBeCalledTimes(1);
     expect(fakeHeaders).toBeCalledTimes(1);
+  });
+
+  it('Should render a custom garden item', async () => {
+    const { gardenModule, fakeGardenMeta, fakeHeaders, gardenConfig } = getMockGardenConfig();
+
+    const uniqueGardenItemId = 'uid-garden-item';
+
+    act(() => {
+      render(
+        <Workspace
+          workspaceOptions={workspaceOptions}
+          modules={[gardenModule]}
+          gardenOptions={{
+            ...gardenConfig,
+            customViews: {
+              customItemView: memo(() => <div id={uniqueGardenItemId}></div>),
+            },
+          }}
+        />
+      );
+    });
+
+    //Have to make an artificial timeout to ensure all functions are called.
+    await fakeTimeout();
+
+    const el = document.getElementById(uniqueGardenItemId);
+    if (!el) {
+      throw new Error('Failed to render unique garden item');
+    }
+
+    expect(el).toBeTruthy();
+  });
+  it('Should render a custom garden group', async () => {
+    const { gardenModule, gardenConfig } = getMockGardenConfig();
+
+    const uniqueGardenHeaderId = 'uid-garden-group';
+
+    act(() => {
+      render(
+        <Workspace
+          workspaceOptions={workspaceOptions}
+          modules={[gardenModule]}
+          gardenOptions={{
+            ...gardenConfig,
+
+            customViews: {
+              customHeaderView: memo(() => <div id={uniqueGardenHeaderId}></div>),
+            },
+          }}
+        />
+      );
+    });
+
+    //Have to make an artificial timeout to ensure all functions are called.
+    await fakeTimeout();
+
+    const el = document.getElementById(uniqueGardenHeaderId);
+    if (!el) {
+      throw new Error('Failed to render unique garden group');
+    }
+
+    expect(el).toBeTruthy();
+  });
+  it('Should render the correct display name', async () => {
+    const { gardenModule, gardenConfig } = getMockGardenConfig();
+
+    const uniqueDisplayName = 'uid-display-name';
+
+    act(() => {
+      render(
+        <Workspace
+          workspaceOptions={workspaceOptions}
+          modules={[gardenModule]}
+          gardenOptions={{
+            ...gardenConfig,
+
+            getDisplayName: () => uniqueDisplayName,
+          }}
+        />
+      );
+    });
+
+    //Have to make an artificial timeout to ensure all functions are called.
+    await fakeTimeout();
+
+    const assertion = getByText(document.body, uniqueDisplayName);
+    expect(assertion).toBeTruthy();
   });
 });
