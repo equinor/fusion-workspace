b3c42d85815d93ff6da7b103e88490371b4329c6
MagnusGjosund <maggu@live.no>
2023-08-01 15:06:37 +0200

    refactor: refactor some code

--- packages/ag-grid/src/lib/components/grid.styles.ts
+++ packages/ag-grid/src/lib/components/grid.styles.ts
@@ -1,8 +1,5 @@
-import { tokens } from '@equinor/eds-tokens';
 import styled from 'styled-components';
 
 export const StyledGridWrapper = styled.div`
   width: 100%;
-  /* padding-top: 50px; */
-  border-top: 2px solid ${tokens.colors.ui.background__medium.hex};
 `;
