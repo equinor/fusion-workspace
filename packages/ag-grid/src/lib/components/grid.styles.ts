0079f0da2d75a50eb580fad09ea58b2173881355
MagnusGjosund <maggu@live.no>
2023-08-01 12:58:39 +0200

    chore:

--- packages/ag-grid/src/lib/components/grid.styles.ts
+++ packages/ag-grid/src/lib/components/grid.styles.ts
@@ -1,5 +1,8 @@
+import { tokens } from '@equinor/eds-tokens';
 import styled from 'styled-components';
 
 export const StyledGridWrapper = styled.div`
   width: 100%;
+  /* padding-top: 50px; */
+  border-top: 2px solid ${tokens.colors.ui.background__medium.hex};
 `;
