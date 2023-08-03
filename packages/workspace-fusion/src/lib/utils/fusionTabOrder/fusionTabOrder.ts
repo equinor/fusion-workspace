297e5fe1ff9b81a9ca14efcad976383482082fb4
Gustav Eikaas <guei@equinor.com>
2023-08-03 08:32:01 +0200

    chore: add powerbi to taborder

--- packages/workspace-fusion/src/lib/utils/fusionTabOrder/fusionTabOrder.ts
+++ packages/workspace-fusion/src/lib/utils/fusionTabOrder/fusionTabOrder.ts
@@ -3,6 +3,7 @@ import { Tab } from '@equinor/workspace-react';
 const tabSortOrder = new Map<string, number>();
 tabSortOrder.set('garden', 0);
 tabSortOrder.set('grid', 1);
+tabSortOrder.set('powerbi', 2);
 
 export function sortFusionTabs<TError>(tabs: Tab[]) {
   tabs.sort((a, b) => {
