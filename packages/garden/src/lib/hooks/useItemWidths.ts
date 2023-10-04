6a03a1462804a38aa1fb91a8be0a811bb0b84933
espenkalle <eskal@equinor.com>
2023-10-04 12:19:25 +0200

    feat: :sparkles: columnwidth in gardenmeta

--- packages/garden/src/lib/hooks/useItemWidths.ts
+++ packages/garden/src/lib/hooks/useItemWidths.ts
@@ -1,16 +1,10 @@
 import { useEffect, useState } from 'react';
 
-export const defaultGardenPackageWidth = 300;
-
-export function useItemWidths(columnCount: number) {
+export function useItemWidths(columnCount: number, itemWidth: number) {
   const [widths, setWidths] = useState<number[]>([]);
-
-  /**
-   * How to calculate longest width?
-   */
   useEffect(() => {
     if (columnCount > 0) {
-      setWidths(new Array(columnCount).fill(defaultGardenPackageWidth));
+      setWidths(new Array(columnCount).fill(itemWidth));
     }
   }, [columnCount]);
 
