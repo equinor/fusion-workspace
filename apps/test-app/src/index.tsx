8ae0d5d06c09ce3a7c091c6c5e61b86dc9ec77c2
espenkalle <eskal@equinor.com>
2023-10-09 11:11:28 +0200

    fix: :bug: removed "scroll-bar" height css.

--- apps/test-app/src/index.tsx
+++ apps/test-app/src/index.tsx
@@ -102,6 +102,76 @@ export function App() {
               filterItems: [{ count: 1, value: 'abc' }],
               isQuickFilter: true,
             },
+            {
+              name: 'item1',
+              filterItems: [{ count: 1, value: 'value1' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item2',
+              filterItems: [{ count: 1, value: 'value2' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item3',
+              filterItems: [{ count: 1, value: 'value3' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item4',
+              filterItems: [{ count: 1, value: 'value4' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item5',
+              filterItems: [{ count: 1, value: 'value5' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item6',
+              filterItems: [{ count: 1, value: 'value6' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item7',
+              filterItems: [{ count: 1, value: 'value7' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item8',
+              filterItems: [{ count: 1, value: 'value8' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item9',
+              filterItems: [{ count: 1, value: 'value9' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item10',
+              filterItems: [{ count: 1, value: 'value10' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item11',
+              filterItems: [{ count: 1, value: 'value11' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item12',
+              filterItems: [{ count: 1, value: 'value12' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item13',
+              filterItems: [{ count: 1, value: 'value13' }],
+              isQuickFilter: true,
+            },
+            {
+              name: 'item14',
+              filterItems: [{ count: 1, value: 'value14' }],
+              isQuickFilter: true,
+            },
           ],
         },
       }}
