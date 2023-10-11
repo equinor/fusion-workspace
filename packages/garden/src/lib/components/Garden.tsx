5b7e881d885a56fa2e1fddee280d98663ac2bbff
Gustav Eikaas <guei@equinor.com>
2023-10-11 08:57:57 +0200

    refactor: grouping selector

--- packages/garden/src/lib/components/Garden.tsx
+++ packages/garden/src/lib/components/Garden.tsx
@@ -1,7 +1,8 @@
-import { Icon } from '@equinor/eds-core-react';
-import { Suspense, useRef } from 'react';
+import { Button, Icon } from '@equinor/eds-core-react';
+import { Suspense, useRef, useState } from 'react';
 import {
   CustomVirtualViews,
+  GardenConfig,
   GardenGroup,
   GardenHeaderGroup,
   GardenMeta,
@@ -14,7 +15,7 @@ import {
 } from '../types';
 import { VirtualContainer } from './VirtualContainer/VirtualContainer';
 
-import { chevron_down, chevron_up } from '@equinor/eds-icons';
+import { arrow_back_ios, arrow_forward_ios, chevron_down, chevron_up } from '@equinor/eds-icons';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SplashScreen } from './splashScreen/SplashScreen';
 import { ErrorBoundary } from 'react-error-boundary';
@@ -22,6 +23,8 @@ import { GardenError } from './error/GardenError';
 import { GetIdentifier } from '../types/getIdentifier';
 import { GardenConfigProvider } from '../context/gardenConfig';
 import { GardenContextProvider } from '../context/gardenContext';
+import styled from 'styled-components';
+import { GroupingSelector } from './GroupingSelector';
 
 export type GardenMetaRequest = {
   groupingKeys: string[];
@@ -57,41 +60,105 @@ export function Garden<TData extends Record<PropertyKey, unknown>, TContext = un
   getDisplayName,
   context,
   getIdentifier,
-  groupingKeys,
+  groupingKeys: initialGrouping,
   customViews,
-  timeInterval,
-  dateVariant,
+  timeInterval: initialTimeInterval,
+  dateVariant: initialDateVariant,
   visuals,
   clickEvents,
   selected = null,
 }: GardenProps<TData, TContext>): JSX.Element | null {
   const client = useRef(new QueryClient());
+  const [groupingKeys, setGroupingKeys] = useState<string[]>(initialGrouping);
+  const [toggle, setToggle] = useState(false);
+
+  const [timeInterval, updateTimeInterval] = useState<string | null>(initialTimeInterval ?? null);
+  const onChangetimeInterval = (timeInterval: string | null) => {
+    updateTimeInterval(timeInterval);
+  };
+  const [dateVariant, updateDateVariant] = useState<string | null>(initialDateVariant ?? null);
+  const onChangeDateVariant = (dateVariant: string | null) => {
+    updateDateVariant(dateVariant);
+  };
 
   return (
     <QueryClientProvider client={client.current}>
       <ErrorBoundary FallbackComponent={GardenError}>
-        <GardenContextProvider
-          getIdentifier={getIdentifier}
-          timeInterval={timeInterval}
-          dateVariant={dateVariant}
-          initialGrouping={groupingKeys}
-          selected={selected}
-        >
-          <GardenConfigProvider
-            dataSource={dataSource}
-            getDisplayName={getDisplayName}
+        <Suspense fallback={<SplashScreen />}>
+          <GardenContextProvider
             getIdentifier={getIdentifier}
-            clickEvents={clickEvents}
-            context={context}
-            customViews={customViews}
-            visuals={visuals}
+            timeInterval={timeInterval}
+            dateVariant={dateVariant}
+            initialGrouping={groupingKeys}
+            selected={selected}
           >
-            <Suspense fallback={<SplashScreen />}>
+            <GardenConfigProvider
+              dataSource={dataSource}
+              getDisplayName={getDisplayName}
+              getIdentifier={getIdentifier}
+              clickEvents={clickEvents}
+              context={context}
+              customViews={customViews}
+              visuals={visuals}
+            >
               <VirtualContainer context={context as TContext} dataSource={dataSource} />
-            </Suspense>
-          </GardenConfigProvider>
-        </GardenContextProvider>
+            </GardenConfigProvider>
+            <StyledAnimatedDiv expanded={toggle}>
+              {toggle ? (
+                <>
+                  <div
+                    style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-between' }}
+                  >
+                    <Button variant="ghost_icon" onClick={() => setToggle((state) => !state)}>
+                      <Icon data={arrow_forward_ios}></Icon>
+                    </Button>
+                    <p style={{ marginRight: '8px', fontSize: '16px', fontWeight: '500' }}>View Settings</p>
+                  </div>
+                  <GroupingSelector
+                    groupingKeys={groupingKeys}
+                    setGroupingKeys={setGroupingKeys}
+                    timeInterval={timeInterval}
+                    onChangeTimeInterval={onChangetimeInterval}
+                    dateVariant={dateVariant}
+                    onChangeDateVarient={onChangeDateVariant}
+                    context={context as any}
+                    dataSource={dataSource}
+                  />
+                </>
+              ) : (
+                <>
+                  <Button variant="ghost_icon" onClick={() => setToggle((state) => !state)}>
+                    <Icon data={arrow_back_ios}></Icon>
+                  </Button>
+                  <VerticalText>View Settings</VerticalText>
+                </>
+              )}
+            </StyledAnimatedDiv>
+          </GardenContextProvider>
+        </Suspense>
       </ErrorBoundary>
     </QueryClientProvider>
   );
 }
+
+type StyledAnimatedDivProps = {
+  expanded: boolean;
+};
+
+const StyledAnimatedDiv = styled.div<StyledAnimatedDivProps>`
+  width: ${(props) => (props.expanded ? '300px' : '45px')};
+  box-shadow: -2px 0px 5px rgba(0, 0, 0, 0.1);
+  margin-left: 10px;
+  padding: 10px;
+  transition: width 0.1s ease;
+  overflow: hidden;
+`;
+const VerticalText = styled.div`
+  transform: rotate(90deg);
+  white-space: nowrap;
+  display: flex;
+  align-items: center;
+  margin-top: 3rem;
+  justify-content: center;
+  font-size: 16px;
+`;
