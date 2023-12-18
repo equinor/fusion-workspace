192021160343046bb35e696aee83a6126ff6863c
Gustav Eikaas <guei@equinor.com>
2023-12-18 14:23:02 +0100

    ci

---
+++ .github/helpers/src/issue-checker.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { getOctokit, context } from '@actions/github';
+import { readFileSync } from 'fs';
+
+const program = new Command();
+type Octo = ReturnType<typeof getOctokit>;
+
+program.name('PR');
+
+program
+  .command('issue')
+  .option('-T, --token <token>', 'github token')
+  .action(async (args) => {
+    if (!args.token) {
+      throw new Error('Missing github token');
+    }
+
+    const client = getOctokit(args.token);
+    checkIssues(client, context.issue.number);
+  });
+
+await program.parseAsync();
+
+async function checkIssues(client: Octo, pr: number) {
+  console.log(`querying pr: ${pr}`);
+  const pullRequests = await client.graphql({
+    request: {
+      fetch: `query {
+    repository (owner: ${context.repo.owner}, name: ${context.repo.repo}){
+   pullRequest (number: ${pr}) {
+     closingIssuesReferences (first: 1){
+       totalCount
+     }
+   }
+ }
+}
+  `,
+    },
+  });
+
+  console.log(pullRequests);
+}
