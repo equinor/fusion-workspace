90588c67404b474d8cd4b63e70d2f8444bc757f9
Gustav Eikaas <guei@equinor.com>
2023-12-18 19:27:43 +0100

    ci:

--- .github/helpers/src/issue-checker.ts
+++ .github/helpers/src/issue-checker.ts
@@ -8,13 +8,19 @@ type Octo = ReturnType<typeof getOctokit>;
 
 program.name('PR');
 
-const noLinkedIssueMessage = `⚠👮‍♀️🚔🚨
-Looks like you forgot to link an issue!
+const noLinkedIssueMessage = `
+Howdy, wanderer,
 
-If you have never linked an issue to a pull request or you just forgot how to. [Read this](https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue)
+Seems you've sauntered into our GitHub saloon with a pull request, but it appears you've forgotten to tie your horse to the hitching post. Now, in this town, we don't take kindly to stray requests, and the GitHub corral is no place for them.
 
+I reckon you best mosey on over and link that pull request to an issue, lest you want the winds of open source trouble blowin' your way. I've got my eye on you, stranger, and a stern warning echoes through these digital canyons.
 
-Next time this violation occurs your teamlead will be notified🔪🩸
+Now, for those who might be new to these parts or sufferin' from a bout of forgetfulness, fear not. I've rustled up a guide that's as handy as a snake in a boot🐍🥾. Take a peek at [this guide](https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue), and it'll show you the way to tether that pull request like a seasoned rancher🤠.
+
+Don't let the sun set on your unlinked pull request, and remember, in these GitHub lands, the code speaks louder than six-shooters.
+
+Sincerely,
+The code patrol👮
 `;
 
 program
