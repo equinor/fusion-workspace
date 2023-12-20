d77bc7982afbee351c62a74618be21a6a17ac486
Gustav Eikaas <guei@equinor.com>
2023-12-20 15:01:43 +0100

    revert: :rewind:

--- .github/helpers/src/issue-checker.ts
+++ .github/helpers/src/issue-checker.ts
@@ -2,7 +2,6 @@
 
 import { Command } from 'commander';
 import { getOctokit, context } from '@actions/github';
-import { setSecret } from '@actions/core';
 
 const program = new Command();
 type Octo = ReturnType<typeof getOctokit>;
@@ -37,37 +36,29 @@ program
       throw new Error('Missing pr number');
     }
 
-    setSecret(args.token);
-
     const client = getOctokit(args.token);
-    checkIssues(client, parseInt(args.pr), args.token);
+    checkIssues(client, args.pr);
   });
 
 await program.parseAsync();
 
-async function checkIssues(client: Octo, pr: number, token: string) {
-  const pullRequests = await client.graphql({
-    query: `query($owner: String!, $name: String!, $number: Int!) {
-      repository (owner: $owner, name: $name){
-     pullRequest (number: $number) {
-       closingIssuesReferences (first: 1){
-         totalCount
-       }
+async function checkIssues(client: Octo, pr: number) {
+  const pullRequests = await client.graphql(
+    `query {
+    repository (owner: "${context.repo.owner}", name: "${context.repo.repo}"){
+   pullRequest (number: ${pr}) {
+     closingIssuesReferences (first: 1){
+       totalCount
      }
    }
-  }
-  `,
-    owner: context.repo.owner,
-    name: context.repo.repo,
-    number: pr,
-    headers: {
-      authorization: token,
-    },
-  });
+ }
+}
+`
+      .replaceAll('\n', '')
+      .trim()
+  );
 
   const linkedIssues: number = (pullRequests as any).repository.pullRequest.closingIssuesReferences.totalCount;
-
-  console.log(JSON.stringify(pullRequests));
 
   if (linkedIssues === 0) {
     const comment = await client.rest.issues.createComment({
