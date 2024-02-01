ada8f7e0d4d96b6869be7f4071f1464104f0c0dd
Espen Kalleberg <108390105+espenkalle@users.noreply.github.com>
2024-02-01 09:23:52 +0100

    Fix/sidebar padding (#553)
    * fix: :bug: Fixed sidebar padding when closed
    
    * this should not be there :)
    
    * resolve

--- packages/garden/src/lib/components/ViewSettings/viewSettings.styles.ts
+++ packages/garden/src/lib/components/ViewSettings/viewSettings.styles.ts
@@ -9,7 +9,7 @@ export const StyledViewSettings = styled.div<StyledViewSettingsProps>`
   width: ${(props) => (props.expanded ? '260px' : '45px')};
   box-shadow: -2px 0px 5px rgba(0, 0, 0, 0.1);
   margin-left: 2px;
-  padding: 10px;
+  padding: ${(props) => (props.expanded ? '10px 0' : '0px')};
   transition: width 0.1s ease;
   overflow: hidden;
   background-color: ${tokens.colors.ui.background__light.rgba};
