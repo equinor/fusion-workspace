cc64d20ad00db0ae809ba21b3d5cc563ba303de0
Gustav Eikaas <guei@equinor.com>
2024-05-23 12:34:01 +0200

    testing bootstrap (hacky code)

--- packages/power-bi/src/lib/components/PowerBi.tsx
+++ packages/power-bi/src/lib/components/PowerBi.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useRef } from 'react';
 import { Loading } from './loading';
 import { chevron_down, chevron_up } from '@equinor/eds-icons';
 import { Icon } from '@equinor/eds-core-react';
@@ -11,12 +11,6 @@ import { Report } from './report/Report';
 
 import React from 'react';
 import { models, factories, service } from 'powerbi-client';
-
-new service.Service(factories.hpmFactory, factories.wpmpFactory, factories.routerFactory).preload({
-  embedUrl: "https://app.powerbi.com/reportEmbed",
-  type: "report",
-
-})
 
 Icon.add({ chevron_down, chevron_up });
 
@@ -37,9 +31,31 @@ export interface PowerBiProps {
 const client = new QueryClient();
 
 export const PowerBi = (props: PowerBiProps) => {
+  const isBootstrapped = useRef(false);
   const { reportUri, ErrorComponent } = props;
   return (
     <QueryClientProvider client={client}>
+      <div id='pbi-bootstrap' style={{ visibility: "hidden", position: "absolute", top: 0, right: 0, width: 0, height: 0 }} ref={(ref) => {
+        if (!ref || isBootstrapped.current == true) return
+        new service.Service(factories.hpmFactory, factories.wpmpFactory, factories.routerFactory).bootstrap(ref, {
+          embedUrl: "https://app.powerbi.com/reportEmbed",
+          type: "report",
+          settings: {
+            panes: {
+              filters: {
+                expanded: false,
+                visible: false,
+              },
+              pageNavigation: {
+                visible: false,
+              },
+            },
+          },
+          tokenType: 1,
+        })
+        isBootstrapped.current = true;
+
+      }} />
       <Suspense fallback={<Loading />}>
         <QueryErrorResetBoundary>
           {({ reset }) => (
