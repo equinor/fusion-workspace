5b3497466e4698a204f71d1d279c356ecd08bac6
Edward Brunton <42774600+EdwardBrunton@users.noreply.github.com>
2024-08-15 10:24:13 +0200

    feat(handover): Added grouping to status bar (#633)
    Co-authored-by: Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>

--- packages/workspace-fusion/src/lib/integrations/status-bar/components/StatusBar.tsx
+++ packages/workspace-fusion/src/lib/integrations/status-bar/components/StatusBar.tsx
@@ -1,6 +1,6 @@
 import { StatusItem } from '../types/statusItem';
 import { StatusBarItem } from './StatusBarItem';
-import { StyledStatusBar } from './statusBar.styles';
+import { StyledStatusBar, StyledStatusBarDivider } from './statusBar.styles';
 
 interface StatusBarProps {
   items: StatusItem[];
@@ -9,10 +9,43 @@ interface StatusBarProps {
 export function StatusBar({ items }: StatusBarProps): JSX.Element | null {
   if (!items.length) return null;
 
+  // Check if all items are in the same group
+  if (items.every((i) => i.group === items[0].group)) {
+    return (
+      <StyledStatusBar id="status_bar_root">
+        {items.map((item) => (
+          <StatusBarItem key={item.title} item={item} />
+        ))}
+      </StyledStatusBar>
+    );
+  }
+
+  // Group items by their 'group' property
+  const groupedItems = items.reduce(
+    (groups, item) => {
+      const group = groups[item.group ?? ''] || [];
+      group.push(item);
+      groups[item.group ?? ''] = group;
+      return groups;
+    },
+    {} as Record<string, StatusBarProps['items']>
+  );
+
+  // Render each group with its items
   return (
     <StyledStatusBar id="status_bar_root">
-      {items.map((item) => (
-        <StatusBarItem key={item.title} item={item} />
+      {Object.entries(groupedItems).map(([groupName, groupItems], i) => (
+        <>
+          {i !== 0 && (
+            <>
+              <StyledStatusBarDivider></StyledStatusBarDivider>
+              {!groupName ? <></> : <div style={{ alignSelf: 'center' }}>{groupName}:</div>}
+            </>
+          )}
+          {groupItems.map((item) => (
+            <StatusBarItem key={item.title} item={item} />
+          ))}
+        </>
       ))}
     </StyledStatusBar>
   );
