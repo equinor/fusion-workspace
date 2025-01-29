700b515300e5dd78b639fc86e68c5028ea7d3761
Edward Brunton <42774600+EdwardBrunton@users.noreply.github.com>
2025-01-29 13:20:10 +0100

    fix: update getFilterHeaderText logic

--- packages/filter/src/lib/utils/getFilterHeaderText.tsx
+++ packages/filter/src/lib/utils/getFilterHeaderText.tsx
@@ -3,13 +3,10 @@ import { StyledNormalText } from '../components/filterGroup/filterGroup.styles';
 
 export function getFilterHeaderText(isAllChecked: boolean, name: string, checkedValues: string[]): JSX.Element {
   const selectedCount = checkedValues.length;
-  const displayText = selectedCount > 0 ? `${name} (+${selectedCount})` : name;
-
-  const textComponent = <StyledNormalText>{displayText}</StyledNormalText>;
 
   return isAllChecked || selectedCount === 0 ? (
-    textComponent
+    <StyledNormalText>{name}</StyledNormalText>
   ) : (
-    <div style={{ color: tokens.colors.interactive.primary__resting.hex }}>{displayText}</div>
+    <div style={{ color: tokens.colors.interactive.primary__resting.hex }}>{`${name} (+${selectedCount})`}</div>
   );
 }
