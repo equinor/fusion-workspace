547aeb9afe1b6ecd7266959869d80865a7fb6325
Karolina Slazyk <76835138+kslazykv@users.noreply.github.com>
2025-01-29 11:50:52 +0100

    Removed blank (#660)

--- packages/filter/src/lib/utils/getFilterHeaderText.tsx
+++ packages/filter/src/lib/utils/getFilterHeaderText.tsx
@@ -1,29 +1,15 @@
 import { tokens } from '@equinor/eds-tokens';
 import { StyledNormalText } from '../components/filterGroup/filterGroup.styles';
 
-export function getFilterHeaderText(
-  isAllChecked: boolean,
-  name: string,
-  checkedValues: string[]
-): string | JSX.Element {
+export function getFilterHeaderText(isAllChecked: boolean, name: string, checkedValues: string[]): JSX.Element {
   const selectedCount = checkedValues.length;
-
-  const hasBlank = checkedValues.includes('(Blank)');
-  const nonBlankCount = hasBlank ? selectedCount - 1 : selectedCount;
-
-  if (hasBlank && nonBlankCount > 0) {
-    return <StyledNormalText>{`${name} (Blank) (+${nonBlankCount})`}</StyledNormalText>;
-  }
-
-  if (selectedCount === 1 && hasBlank) {
-    return <StyledNormalText>{`${name} (Blank)`}</StyledNormalText>;
-  }
-
   const displayText = selectedCount > 0 ? `${name} (+${selectedCount})` : name;
 
-  if (isAllChecked || selectedCount === 0) {
-    return <StyledNormalText>{displayText}</StyledNormalText>;
-  }
+  const textComponent = <StyledNormalText>{displayText}</StyledNormalText>;
 
-  return <div style={{ color: tokens.colors.interactive.primary__resting.hex }}>{displayText}</div>;
+  return isAllChecked || selectedCount === 0 ? (
+    textComponent
+  ) : (
+    <div style={{ color: tokens.colors.interactive.primary__resting.hex }}>{displayText}</div>
+  );
 }
